71415610bf66ca04dd77a67369845689342fb64d
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-10-10 19:57:54 -0400

    Added front end AJAX communication experiment

---
+++ PLCFrontEnd/server.js
@@ -0,0 +1,47 @@
+var http = require('http'),
+      fs = require('fs'),
+     url = require('url'),
+ choices = ["hello world", "goodbye world"];
+
+var filename = 'clientList.tbl'
+
+var parseStuff = function(path, response)
+{
+	var truePath = path;
+	
+	if(path == "/")
+	{
+		truePath = "/index.html";
+	}
+	
+	console.log("True Path " + truePath);
+	
+	fs.readFile("." + truePath, function(err, file) {  
+            if(err) {  
+                // write an error response or nothing here  
+                return;  
+            }  
+            //response.writeHead(200, { 'Content-Type': 'text/html' });  
+            response.end(file, "utf-8");
+        });
+}
+
+http.createServer(function(request, response){
+    var path = url.parse(request.url).pathname;
+	console.log("Path is " + path);
+    if(path=="/getstring")
+    {
+        console.log("request recieved");
+        var string = fs.readFile(filename, 'utf8', function(err, data) {
+            if (err) throw err;
+            console.log('OK: ' + filename);
+            console.log(data);
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.end(data, "utf-8");
+        });
+    }
+    else{
+		parseStuff(path, response);
+    }
+}).listen(8001);
+console.log("server initialized");
