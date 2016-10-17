07bff168df2bcda280f4e8a51192320920f3816e
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-10-17 15:48:31 -0400

    Removed extra code from TestClientClass.py

--- Client/TestClientClass.py
+++ Client/TestClientClass.py
@@ -2,15 +2,3 @@ from ClientConnection import ClientConnection
 
 client = ClientConnection()
 client.Connect()
-
-#import time
-#from threading import Thread
-#
-#def myfunc(i):
-#    print "sleeping 5 sec from thread %d" % i
-#    time.sleep(5)
-#    print "finished sleeping from thread %d" % i
-#
-#for i in range(10):
-#    t = Thread(target=myfunc, args=(i,))
-#    t.start()
