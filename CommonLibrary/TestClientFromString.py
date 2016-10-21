bd7c8ef9a5edd8acac64d55dbc626955e194274e
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-10-21 01:15:29 -0400

    Vastly simplified the TestClientFromString script

--- CommonLibrary/TestClientFromString.py
+++ CommonLibrary/TestClientFromString.py
@@ -2,17 +2,6 @@ from PLCClient import PLCClient
 from ControlValue import ControlValue
 import json
 
-client = PLCClient()
-client.SetClientFromString('192.168.1.1,2,Kadelope,0,Mexico,0,Torid,1,Jao,0,100')
-
-print client.IPAddress
-print client.ControlList
-
-for x in range(0, len(client.ControlList)) :
-	print client.ControlList[x].ControlName
-
-print client.GetClientString()
-
 otherClient = PLCClient()
 otherClient.ClientName = 'Sloppy'
 Control1 = ControlValue()
@@ -22,23 +11,13 @@ Control2.ControlName = 'Fast'
 
 otherClient.ControlList.append(Control1)
 otherClient.ControlList.append(Control2)
+
 test = otherClient.toJSON()
-print test
-
 newClient = json.loads(test)
-print newClient
 
-anotherClient = PLCClient()
-anotherClient.fromJSON(test)
-print 'here'
-print anotherClient.ClientName
-print anotherClient.ControlList[1].ControlName
-
-
-newClient = json.loads(client.toJSON())
+newClient = json.loads(otherClient.toJSON())
 with open('Test.json', 'w') as f:
 	json.dump(newClient, f, sort_keys=True, indent=4, separators=(',', ': '))
-print newClient
 f.close()
 
 try :
@@ -48,10 +27,9 @@ try :
 except :
 	print 'No client JSON file found'
 
-print 'Here'
-print allExistingClients
 aaaClient = PLCClient()
 aaaClient.fromDict(allExistingClients)
-print aaaClient.ClientName
-print aaaClient.ControlList[0].ControlName
-print aaaClient.GetClientString()
+
+print 'These should be the same'
+print aaaClient.ControlList[1].ControlName
+print otherClient.ControlList[1].ControlName
