e28e680f2e0535116cd1f676491aedb5cd9dd46c
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-10-26 09:18:07 -0400

    Fixed #38 Begin adding unit test coverage to the PLCClient object

--- CommonLibrary/TestPLCClient.py
+++ CommonLibrary/TestPLCClient.py
@@ -1,6 +1,12 @@
 from PLCClient import PLCClient
 from ControlValue import ControlValue
 
+def CheckEquality(first, second, testName) :
+	if first == second :
+		return testName + ': Success'
+	return testName + ': Failed'
+
+# Create Controls and PLC Client
 otherClient = PLCClient()
 otherClient.ClientName = 'Sloppy'
 Control1 = ControlValue()
@@ -10,23 +16,22 @@ Control2.ControlName = 'Fast'
 Control2.ControlValueType = Control2.OnOff
 otherClient.ControlList.append(Control1)
 otherClient.ControlList.append(Control2)
+print CheckEquality(otherClient.ClientName, 'Sloppy', 'ClientNameSetTest')
 
-print otherClient
+# Extract, Create, and Update Queued Commands
 firstCtrl = otherClient.ControlList[1]
 firstCtrl.CurCtrlValue = 40
 otherClient.QueueControl(firstCtrl)
-
 curCtrl = otherClient.ControlList[0]
 curCtrl.CurCtrlValue = 3
 otherClient.QueueControl(curCtrl)
 curCtrl.CurCtrlValue = 5
 otherClient.QueueControl(curCtrl)
 otherClient.QueueControlByName('Forge', 210)
+print CheckEquality(otherClient.GetQueuedControlString(), 'Fast,40;Forge,210;', 'QueueControlByNameTest')
+print CheckEquality(otherClient.GetQueuedControlString(), '', 'GetQueuedControlStringTest')
 
-print otherClient.GetQueuedControlString()
-print otherClient.GetQueuedControlString()
-
+# Update controls on a client by string
 otherClient.UpdateControlsByString('Fast,20;Forge,30;')
-
-print otherClient.ControlList[0].CurCtrlValue
-print otherClient.ControlList[1].CurCtrlValue
+print CheckEquality(otherClient.ControlList[0].CurCtrlValue, 30, 'UpdateControlsByStringTest1')
+print CheckEquality(otherClient.ControlList[1].CurCtrlValue, 20, 'UpdateControlsByStringTest2')
