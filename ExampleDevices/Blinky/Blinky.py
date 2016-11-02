26711f6ba94ed65e2a87b2865366f50652c5fd0f
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-11-02 15:25:47 -0400

    Made interrupt based blinky

--- ExampleDevices/Blinky/Blinky.py
+++ ExampleDevices/Blinky/Blinky.py
@@ -39,11 +39,12 @@ client.Connect()
 client.SendClientDataToServer()
 
 counter = 0
+client.StartInterruptServerThread()
 print 'Waiting for changes'
 while 1 :
-	time.sleep(0.5)
-	commandStr = client.GetQueuedCommandsFromServer()
-	if len(commandStr) > 0:
-		print 'Command Received: ', commandStr
-		client.clientData.UpdateControlsByString(commandStr)
-		ToggleLight(client.clientData.ControlList[0].CurCtrlValue)
+	# time.sleep(0.5)
+	# commandStr = client.GetQueuedCommandsFromServer()
+	# if len(commandStr) > 0:
+		# print 'Command Received: ', commandStr
+		# client.clientData.UpdateControlsByString(commandStr)
+	ToggleLight(client.clientData.ControlList[0].CurCtrlValue)
