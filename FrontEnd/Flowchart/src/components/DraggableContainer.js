124a9831f4b616b075d400a9a707702ca2315c92
Jacob Keith <jacobkeith42@gmail.com>
2016-11-09 17:58:02 -0500

    #82 the vertex connection now follows the client card around as it is dragged

--- FrontEnd/Flowchart/src/components/DraggableContainer.js
+++ FrontEnd/Flowchart/src/components/DraggableContainer.js
@@ -27,10 +27,25 @@ class DraggableContainer extends React.Component {
 		this.vertex['destIP'] = destIP;
 		this.vertex['destID'] = destID;
 		this.vertex['inputPosition'] = position;
-		const vertexName = this.vertex.sourceID + '->' + this.vertex.destID;
-		const vertexObject = this.state.vertexPaths;
+		
+		var vertexName = this.getVertexNameFromSavedIO();
+		this.commitUpdatedVertexCoordinates(vertexName);
 
+		console.log(this.state.vertexPaths);
+		this.sendVertexToServer();
+
+	}
+
+	getVertexNameFromSavedIO(){
+		return this.vertex.sourceID + '->' + this.vertex.destID;
+	}
+
+	commitUpdatedVertexCoordinates(vertexName) {
+
+		const vertexObject = this.state.vertexPaths;
 		vertexObject[[vertexName]] = {
+								sourceID: this.vertex.sourceID,
+								destID: this.vertex.destID,
 								x1: this.vertex.outputPosition['left'],
 								x2: this.vertex.inputPosition['left'],
 								y1: this.vertex.outputPosition['top'],
@@ -38,18 +53,41 @@ class DraggableContainer extends React.Component {
 							};
 
 		this.setState({vertexPaths: vertexObject});
+	}
 
+	updateVertexPositions(clientID, dragOffset) {
+		console.log("entering updateVertexPositions");
+		console.log(dragOffset);
+		console.log(clientID);
 
+		var currentVertexPaths = this.state.vertexPaths;
+
+		for(var thisVertex in this.state.vertexPaths) {	//iterate through all vertexes
+			
+			if(this.state.vertexPaths[thisVertex]['destID'] == clientID ) { //check if input ID matches
+				
+				console.log('apply offset to position2');
+				currentVertexPaths[thisVertex]['x2'] = currentVertexPaths[thisVertex]['x2'] + dragOffset['left'];
+				currentVertexPaths[thisVertex]['y2'] = currentVertexPaths[thisVertex]['y2'] + dragOffset['top'];
+
+			}
+			else if(this.state.vertexPaths[thisVertex]['sourceID'] == clientID){ //check if output id matches
+				console.log('//apply offset to position1');
+				currentVertexPaths[thisVertex]['x1'] = currentVertexPaths[thisVertex]['x1'] + dragOffset['left'];
+				currentVertexPaths[thisVertex]['y1'] = currentVertexPaths[thisVertex]['y1'] + dragOffset['top'];
+				
+			}
+
+		}
+
+		this.setState({vertexPaths: currentVertexPaths});
 		console.log(this.state.vertexPaths);
-		this.sendVertexToServer();
-
 	}
 
 	selectOutput(outputName, sourceID, position){
 		this.vertex['outputName'] = outputName;
 		this.vertex['sourceID'] = sourceID;
 		this.vertex['outputPosition'] = position;
-		console.log(this.vertex);
 	}
 
 	sendVertexToServer() {
@@ -107,6 +145,7 @@ class DraggableContainer extends React.Component {
 				left: 0,
 				selectInput: (inputName, destIP, destID, position) => this.selectInput(inputName, destIP, destID, position),
 				selectOutput: (outputName, sourceID, position) => this.selectOutput(outputName, sourceID, position),
+				updateVertexPositions: (clientID, dragOffset) => this.updateVertexPositions(clientID, dragOffset),
 			},
 			vertexSVGSpace:{
 				style: styles.vertexSVGSpace
@@ -123,7 +162,7 @@ class DraggableContainer extends React.Component {
 		}
 
 		let clientNodes = [];
-		console.log(this.props.clientList);
+
 		for( var client in this.props.clientList){
 
 			inputs.clientGraphic['key'] = this.props.clientList[client]['ClientID'];
@@ -136,14 +175,13 @@ class DraggableContainer extends React.Component {
 
 		var vertexDrawings = [];
 		for(var thisVertex in this.state.vertexPaths){
-			console.log("entering vertex drawing");
+
 			inputs.vertexSVG['key'] = thisVertex;
 			inputs.vertexSVG['x1'] = this.state.vertexPaths[thisVertex]['x1'];
 			inputs.vertexSVG['x2'] = this.state.vertexPaths[thisVertex]['x2'];
 			inputs.vertexSVG['y1'] = this.state.vertexPaths[thisVertex]['y1'];
 			inputs.vertexSVG['y2'] = this.state.vertexPaths[thisVertex]['y2'];
 
-			console.log(inputs.vertexSVG);
 			vertexDrawings.push(<line {...inputs.vertexSVG}/>);
 		}
 
