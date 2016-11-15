98eb25694ac0f34bb18a15fb9e0e7322c57fae0c
Dylan <dylan.fashbaugh@gmail.com>
2016-11-15 01:02:39 -0500

    Fixed #103 Added first real unit test for client class

--- Firmware/Client/ClientTestProgram.cpp
+++ Firmware/Client/ClientTestProgram.cpp
@@ -44,12 +44,38 @@ void CheckResults(std::string testName, ExpectedValue valueList [], int numberOf
 	OnSuccess(testName);
 }
 
+void TestConstructor()
+{
+	std::string TestName = "Client Name Test";
+
+	Client test(10, "Test");
+
+	std::string ExpectedName = "Test";
+	std::string ReceivedName = test.GetClientName();
+
+	int nameDidPass = 0;
+	if(ExpectedName == ReceivedName)
+	{
+		nameDidPass = 1;
+	}
+
+	ExpectedValue valueList [2];
+	valueList[0].valueName = "Client ID Value";
+	valueList[0].expectedValue = 10;
+	valueList[0].actualValue = test.GetClientID();
+
+	valueList[1].valueName = "Client Name";
+	valueList[1].expectedValue = 1;
+	valueList[1].actualValue = nameDidPass;
+
+	CheckResults(TestName, valueList, 2);
+}
 
 int main(void)
 {
 	cout << "Test Beginning" << endl;
 
-	Client test(10, "Test");
+	TestConstructor();
 
 	return 0;
 }
