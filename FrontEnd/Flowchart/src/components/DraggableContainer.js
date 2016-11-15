9084e78dbdfec2c3a46c1e04a643ec101f688a55
Dylan <dylan.fashbaugh@gmail.com>
2016-11-15 00:05:36 -0500

    Fixed #103 Created first firmware unit test test program

--- FrontEnd/Flowchart/src/components/DraggableContainer.js
+++ FrontEnd/Flowchart/src/components/DraggableContainer.js
@@ -28,7 +28,7 @@ class DraggableContainer extends React.Component {
 
 	componentDidMount(){
 
-		setTimeout(this.fillVertexesFromClientlist.bind(this), 1);
+		setTimeout(this.fillVertexesFromClientlist.bind(this), 1000);
 
 	}
 
