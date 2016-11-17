8768ca1818f1e424913fd261ba135e04a61741cc
Dylan <dylan.fashbaugh@gmail.com>
2016-11-17 15:55:49 -0500

    Fixed #103 Added client IP and string differences for PC and Arduino

--- Firmware/Client/Client.cpp
+++ Firmware/Client/Client.cpp
@@ -5,11 +5,25 @@ Client::Client()
 
 }
 
-Client::Client(int ID, char* name)
+#ifdef ONPC
+Client::Client(int ID, std::string name)
 	: clientID(ID)
 	, clientName(name)
+	, clientType(0)
+	, clientIP("None")
 {
 }
+
+#else
+Client::Client(int ID, String name)
+	: clientID(ID)
+	, clientName(name)
+	, clientType(0)
+	, clientIP("None")
+{
+}
+
+#endif
 
 Client::~Client()
 {
--- Firmware/Client/Client.h
+++ Firmware/Client/Client.h
@@ -1,14 +1,39 @@
+#include "../Utilities/GlobalDefines.h"
+
+#ifdef ONPC
+#include <string>
+#endif
+
 class Client {
 public:
 	Client();
-	Client(int ID, char* name);
+
+#ifdef ONPC
+	Client(int ID, std::string name);
+#else
+	Client(int ID, String name)
+#endif
 	~Client();	
 
 	// Getters
-	char* 	GetClientName() {return clientName; };
 	int 	GetClientID() 	{return clientID; };
+
+#ifdef ONPC
+	std::string GetClientName() {return clientName; };
+#else
+	String 		GetClientName() {return clientName; };
+#endif
 
 private:
 	int clientID;
-	char* clientName;
+	int clientType;
+
+#ifdef ONPC
+	std::string clientName;
+	std::string clientIP;
+#else
+	String clientName;
+	String clientIP;
+#endif
+
 };
