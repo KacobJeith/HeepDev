18b3dad7465a9b6e9f42cd5f521d5524dce516f6
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-11-19 17:24:03 -0500

    #112 Added constructor definitions. Added code to allow PC and Arduino code to run

--- Firmware/Vertex/Vertex.h
+++ Firmware/Vertex/Vertex.h
@@ -8,13 +8,27 @@ class Vertex {
 public:
 
 	Vertex();
+#ifdef ONPC
+	Vertex(std::string inputStr, std::string outputStr, std::string destIP, int destID, int srcID);
+	Vertex(std::string VertexString);
+#else
+	Vertex(String inputStr, String outputStr, String destIP, int destID, int srcID);
+	Vertex(String VertexString);
+
+#endif
 	~Vertex();
 
 private:
+#ifdef ONPC
 	std::string inputName;
 	std::string outputName;
-	int destinationID;
 	std::string destinationIP;
+#else
+	String inputName;
+	String outputName;
+	String destinationIP;
+#endif
+	int destinationID;
 	int sourceID;
 };
 
