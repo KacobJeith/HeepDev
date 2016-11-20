943e03af3c38e3093cabdea1d6924add0e3456c8
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-11-19 19:33:13 -0500

    #112 Added vertex by string test

--- Firmware/Vertex/VertexTestProgram.cpp
+++ Firmware/Vertex/VertexTestProgram.cpp
@@ -89,6 +89,54 @@ void TestVertexListSize()
 	CheckResults(TestName, valueList, 1);
 }
 
+void TestVertexByString()
+{
+	std::string TestName = "Vertex By String Test";
+
+	Vertex vert("input,output,myIP,12,34");
+
+	std::string expectedInput = "input";
+	std::string actualInput = vert.GetInputName();
+	int isInputNameCorrect = 0;
+	if(expectedInput == actualInput)
+		isInputNameCorrect = 1;
+
+	std::string expectedOutput = "output";
+	std::string actualOutput = vert.GetOutputName();
+	int isOutputNameCorrect = 0;
+	if(expectedOutput == actualOutput)
+		isOutputNameCorrect = 1;
+
+	std::string expectedIP = "myIP";
+	std::string actualIP = vert.GetDestinationIP();
+	int isIPCorrect = 0;
+	if(expectedIP == actualIP)
+		isIPCorrect = 1;
+
+	ExpectedValue valueList [5];
+	valueList[0].valueName = "Input Name";
+	valueList[0].expectedValue = 1;
+	valueList[0].actualValue = isInputNameCorrect;
+
+	valueList[1].valueName = "Output Name";
+	valueList[1].expectedValue = 1;
+	valueList[1].actualValue = isOutputNameCorrect;
+
+	valueList[2].valueName = "Destination IP";
+	valueList[2].expectedValue = 1;
+	valueList[2].actualValue = isIPCorrect;
+
+	valueList[3].valueName = "Destination ID";
+	valueList[3].expectedValue = 12;
+	valueList[3].actualValue = vert.GetDestinationID();
+
+	valueList[4].valueName = "Source ID";
+	valueList[4].expectedValue = 34;
+	valueList[4].actualValue = vert.GetSourceID();
+
+	CheckResults(TestName, valueList, 5);
+}
+
 void TestConstructor()
 {
 	std::string TestName = "Vertex Constructor Test";
@@ -143,6 +191,7 @@ int main(void)
 	TestConstructor();
 	TestVertexListSize();
 	TestVertexListGetByIndex();
+	TestVertexByString();
 
 	return 0;
 }
