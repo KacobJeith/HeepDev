5d3532ea61e61ce8625de128076d4fc42b931f30
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-11-19 19:31:19 -0500

    #112 Added get vertex by string constructor

--- Firmware/Vertex/Vertex.cpp
+++ Firmware/Vertex/Vertex.cpp
@@ -75,6 +75,56 @@ Vertex::Vertex(std::string inputStr, std::string outputStr, std::string destIP, 
 {
 
 }
+
+Vertex::Vertex(std::string VertexString)
+{
+	enum parseState {parseInputName, parseOutputName, parseDestIP, parseDestID};
+
+	parseState curState = parseInputName;
+
+	std::string inputStr, outputStr, destIPStr, destIDStr, sourceIDStr;
+	int inputIndex, outputIndex, IPIndex, destIDIndex, sourceIDIndex;
+
+	for(int i = 0; i<VertexString.size(); i++)
+	{
+		char curChar = VertexString[i];
+		
+		if(curChar == ',')
+		{
+			if(curState == parseInputName)
+			{
+				inputIndex = i;
+				inputStr = VertexString.substr(0, inputIndex);
+				curState = parseOutputName;
+			}
+			else if(curState == parseOutputName)
+			{
+				outputIndex = i;
+				outputStr = VertexString.substr(inputIndex+1, outputIndex-(inputIndex+1));
+				curState = parseDestIP;
+			}
+			else if(curState == parseDestIP)
+			{
+				IPIndex = i;
+				destIPStr = VertexString.substr(outputIndex+1, IPIndex-(outputIndex+1));
+				curState = parseDestID;
+			}
+			else if(curState == parseDestID)
+			{
+				destIDIndex = i;
+				destIDStr = VertexString.substr(IPIndex+1, destIDIndex-(IPIndex+1));
+				sourceIDStr = VertexString.substr(destIDIndex+1, VertexString.size()-(destIDIndex+1));
+				break;
+			}
+		}
+	}
+
+	inputName = inputStr;
+	outputName = outputStr;
+	destinationIP = destIPStr;
+	destinationID = std::stoi(destIDStr.c_str());
+	sourceID = std::stoi(sourceIDStr.c_str());
+}
 #else
 Vertex::Vertex(String inputStr, String outputStr, String destIP, int destID, int srcID)
 	: inputName(inputStr)
