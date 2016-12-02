0478d04eb845161e00715bf8a37b23ac136ad127
Dylan <dylan.fashbaugh@gmail.com>
2016-12-02 17:15:15 -0500

    #113 Added client test to connection class

--- Firmware/ClientAPI/ClientConnectionTest.cpp
+++ Firmware/ClientAPI/ClientConnectionTest.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include "ClientConnection.h"
 
 using namespace std;
+
+void TestClientLoad()
+{
+	Client forge(1232, "Crack", 3);
+	ClientConnection conn(forge);
+}
 
 int main(void)
 {
 	cout << "Begin Test" << endl;
+	TestClientLoad();
 
 	return 0;
 }
