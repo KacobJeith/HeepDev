9317d67c726f99d7c7757474a9690700f57ba1fb
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-12-07 23:30:08 -0500

    #119 Updated string utilities in firmware folder

--- Firmware/LowMemoryFirmware/StringUtils.h
+++ Firmware/LowMemoryFirmware/StringUtils.h
@@ -1,10 +1,6 @@
-#ifndef STRING_UTIL_DEF
-#define STRING_UTIL_DEF
-
 void ClearString(char* curString, int bufferLen);
 int GetStringLength(char* str);
 void CopyStringToBuffer(char* dest, char* src);
 void WriteIntToString(int number, char* dest, int &startPoint);
 void CopyStringToBufferAtPos(char* dest, char* src, int &startPoint);
-
-#endif
+int ParseIntFromSubString(char* str, int start, int end);
