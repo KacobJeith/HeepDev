c5d31093d3cc033300fe2bade70b22f597278f57
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-12-08 09:17:21 -0500

    #119 Removed warnings from the test application

--- Firmware/LowMemoryFirmware/TestLowMemoryFirmware.cpp
+++ Firmware/LowMemoryFirmware/TestLowMemoryFirmware.cpp
@@ -12,14 +12,17 @@ int main(void)
 {
 	cout << "Begin Tests" << endl;
 
-	CreateClientFromParams("Test", 1234, 1);
+	char myTest [] = "Test";
+	CreateClientFromParams(myTest, 1234, 1);
 
 	cout << GetClientName() << endl;
 
-	theControl = CreateControl("TestCtrl", 0, 1, 10, 11);
+	char testCtrl1 [] = "TestCtrl";
+	theControl = CreateControl(testCtrl1, 0, 1, 10, 11);
 	AddControlToList(theControl);
 
-	theControl2 = CreateControl("TestingCtrl2", 0, 1, 15, 8);
+	char testCtrl2 [] = "TestCtrl2";
+	theControl2 = CreateControl(testCtrl2, 0, 1, 15, 8);
 	AddControlToList(theControl2);
 
 	cout << (int)controlList[0]->highLim << endl;
