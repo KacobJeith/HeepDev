edc9fe6a7faf297c151889fe044a3a0762af4808
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-12-09 12:53:25 -0500

    #124 Removed the special case parseing for OnOff.

--- CommonLibrary/ControlValue.py
+++ CommonLibrary/ControlValue.py
@@ -20,41 +20,19 @@ class ControlValue:
 		return
  
 	def GetControlValueString(self):
-		if self.ControlValueType == self.OnOff :
-			return self.GetOnOffString()
-		elif self.ControlValueType == self.Range :
-			return self.GetRangeString()
-
-	def GetOnOffString(self) :
-		myString = str(self.ControlValueType) + ',' + self.ControlName
-		return myString
-
-	def GetRangeString(self) :
 		myString = str(self.ControlDirection) + ',' + str(self.ControlValueType) + ',' + self.ControlName + ',' + str(self.LowValue) + ',' + str(self.HighValue)
 		return myString
-
-	def SetOnOffFromSplitString(self, splitString, startIndex) :
-		self.ControlName = splitString[startIndex + 1]
-		return startIndex + 2
-
-	def SetRangeFromSplitString(self, splitString, startIndex) :
-		self.ControlName = splitString[startIndex + 1]
-		self.LowValue = int(splitString[startIndex + 2])
-		self.HighValue = int(splitString[startIndex + 3])
-		return startIndex + 4
 
 	def SetControlFromSplitString(self, splitString, startIndex) :
 		self.ControlDirection = int(splitString[startIndex])
 		self.ControlValueType = int(splitString[startIndex+1])
 		startIndex = startIndex+1
-		newIndex = startIndex + 1
 
-		if self.ControlValueType == self.OnOff :
-			newIndex = self.SetOnOffFromSplitString(splitString, startIndex)
-		elif self.ControlValueType == self.Range :
-			newIndex = self.SetRangeFromSplitString(splitString, startIndex)
-
-		return newIndex
+		# May need to be modified in the future for string inputs
+		self.ControlName = splitString[startIndex + 1]
+		self.LowValue = int(splitString[startIndex + 2])
+		self.HighValue = int(splitString[startIndex + 3])
+		return startIndex + 4
 
 	def PrepareForJSONWrite(self) :
 		self.ControlValueType = self.ControlValueType
