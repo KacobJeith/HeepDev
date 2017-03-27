5433c70c92c3326e21846b0bbb2e71f3c29d5aa5
Jacob Keith <jacobkeith42@gmail.com>
2017-03-27 17:16:27 -0400

    #59 condensing webpack build info into a single config file

--- FrontEnd/Serverless/webpack.config.js
+++ FrontEnd/Serverless/webpack.config.js
@@ -1,23 +1,62 @@
 'use strict';
 
+var path = require('path');
 var webpack = require('webpack');
-var config = require('./webpack.config.base.js');
-/*
-if (process.env.NODE_ENV !== 'test') {
-  config.entry = ['webpack-dev-server/client?http://localhost:3000', 'webpack/hot/dev-server'].concat(config.entry);
-}
 
-config.devtool = '#cheap-module-eval-source-map';
+var NODE_ENV = process.env.NODE_ENV;
 
-config.plugins = config.plugins.concat([new webpack.HotModuleReplacementPlugin()]);
-*/
-config.module.loaders = config.module.loaders.concat(
-	[
-		{ test: /\.jsx?$/, 
-			loaders: ['babel'], 
-			exclude: /node_modules/ 
-		}
-	]
-);
+var env = {
+  production: NODE_ENV === 'production',
+  staging: NODE_ENV === 'staging',
+  test: NODE_ENV === 'test',
+  development: NODE_ENV === 'development' || typeof NODE_ENV === 'undefined'
+};
 
-module.exports = config;
+Object.assign(env, {
+  build: env.production || env.staging
+});
+
+module.exports = {
+  target: 'web',
+
+  entry: ['src/index.js'],
+
+  output: {
+    path: path.resolve(__dirname, "src/dist"),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+
+  resolve: {
+    root: path.join(__dirname, ''),
+    modulesDirectories: ['web_modules', 'node_modules', 'client', 'js'],
+    extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx']
+  },
+
+  plugins: [new webpack.DefinePlugin({
+    __DEV__: env.development,
+    __STAGING__: env.staging,
+    __PRODUCTION__: env.production,
+    __CURRENT_ENV__: '\'' + NODE_ENV + '\''
+  })],
+
+  module: {
+    loaders: [
+      { 
+        test: /\.scss$/, 
+        loader: 'style!css!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded' 
+      },
+      { 
+        exclude: /(node_modules|bower_components)/,
+        text: /\.js$/, 
+        loader: 'babel-loader',
+      },
+      { test: /\.jsx?$/, 
+        loaders: ['babel'], 
+        exclude: /node_modules/ 
+      }
+    ],
+
+    noParse: /\.min\.js/
+  }
+};
