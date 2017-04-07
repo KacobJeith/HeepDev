195588b821a263eb441813eedbae9ddf1364979a
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-04-07 14:09:16 -0400

    #93 Removed last few functions that were repeated in Client Memory

--- CommonLibrary/ClientMemory.py
+++ CommonLibrary/ClientMemory.py
@@ -23,39 +23,8 @@ class ClientMemory:
 	def SetClientName(self, clientName, clientID) :
 		self.miscMemory = self.MemoryUtilities.SetClientName(self.miscMemory, clientName, clientID)
 
-	def GetClientIDFromMemory(self, counter) :
-
-		clientIDAndCounter = self.GetNumberFromMemory(counter, 4)
-
-		return clientIDAndCounter
-
-	def SkipOpCode(self, counter) :
-		counter += 5 # This skips the Client ID and lands on the bytes to skip
-
-		bytesToSkip = ord(self.miscMemory[counter])
-		counter += bytesToSkip + 1
-
-		return counter
-
-	def GetNumberFromMemory(self, counter, numBytes) :
-
-		retVal = 0
-
-		byteCounter = numBytes - 1
-		for x in range(0, numBytes) :
-			curVal = ord(self.miscMemory[counter]) << (8*byteCounter)
-			byteCounter -= 1
-			retVal += curVal
-			counter += 1
-
-		return (retVal, counter)
-
-	def ReadClientNameOpCode(self, counter) :
-		return self.MemoryUtilities.ReadClientNameOpCode(self.miscMemory, counter)
-
 	def GetClientXY(self, clientID) :
 		clientInfo = self.MemoryUtilities.GetClientXYInfo(self.miscMemory, clientID)
-
 		return (clientInfo[2], clientInfo[3])
 
 	def GetClientName(self, clientID) :
