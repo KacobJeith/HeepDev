d25f58b38db86f2578b8eae7aeaaa61bbdcfd212
Jacob Keith <jacobkeith42@gmail.com>
2017-04-13 14:44:12 -0400

    #144 removing jquery from files that no longer use it

--- FrontEnd/Serverless/src/components/OnOffController.js
+++ FrontEnd/Serverless/src/components/OnOffController.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 import Icon from '../assets/icons';
 import {ICONS} from '../assets/iconConstants';
 
--- FrontEnd/Serverless/src/components/RangeController.js
+++ FrontEnd/Serverless/src/components/RangeController.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import $ from 'jquery';
 
 class RangeController extends React.Component {
 
