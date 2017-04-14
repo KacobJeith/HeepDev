c7ae990d8ad73ef4a8d009afd6023d0282ae19fb
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-04-14 14:26:26 -0400

    #159 Changed function to functional in serverless client test

--- ServerlessWorld/ServerlessClient/ServerlessDeviceTest.py
+++ ServerlessWorld/ServerlessClient/ServerlessDeviceTest.py
@@ -7,37 +7,33 @@ from Device import Device
 from Vertex import Vertex
 from CommonDataTypes import HeepIPAddress
 
-functionDevice = Device()
-functionDevice.ClientID = 5678
-functionDevice.SetClientFrontEndXY(100, 287)
-functionDevice.SetIconInformation(1, [chr(3), chr(4), chr(12), chr(41)]) 
-functionDevice.SetClientName("JERCERB")
+functionalDevice = Device()
+functionalDevice.ClientID = 5678
+functionalDevice.SetClientFrontEndXY(100, 287)
+functionalDevice.SetIconInformation(1, [chr(3), chr(4), chr(12), chr(41)]) 
+functionalDevice.SetClientName("JERCERB")
 OnOffControls = ControlValue()
 OnOffControls.ControlValueType = OnOffControls.OnOff
 OnOffControls.ControlDirection = 1
 OnOffControls.ControlName = 'Fig' 
 OnOffControls.ControlID = 2
-functionDevice.ControlList.append(OnOffControls)
+functionalDevice.ControlList.append(OnOffControls)
 
 AnotherControl = ControlValue()
 AnotherControl.ControlValueType = AnotherControl.OnOff
 AnotherControl.ControlDirection = 1
 AnotherControl.ControlName = 'Pretzel'
 AnotherControl.ControlID = 1
-functionDevice.ControlList.append(AnotherControl)
+functionalDevice.ControlList.append(AnotherControl)
 
 Output1 = ControlValue()
 Output1.ControlValueType = Output1.Range
 Output1.ControlName = 'Fruity'
 Output1.ControlID = 0
-functionDevice.ControlList.append(Output1)
-
-functionDevice.SetServerless(1)
-
-# functionalClient.DumpClientMemory()
+functionalDevice.ControlList.append(Output1)
 
 device = ServerlessDeviceConnection()
 
-device.SetClientData(functionalClient)
+device.SetClientData(functionalDevice)
 
 device.StartHeepClientServer()
