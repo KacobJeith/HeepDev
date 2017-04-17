4f4beb6f89c09f03158f243cfe680ff03b80df2e
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-04-17 09:49:15 -0400

    #175 Output data can be sent directly to other clients via vertices

--- ServerlessWorld/ServerlessDevice/ServerlessDevice.py
+++ ServerlessWorld/ServerlessDevice/ServerlessDevice.py
@@ -6,6 +6,7 @@ from ControlValue import ControlValue
 from Device import Device
 from OutputData import OutputData
 from ActionOpCodeParser import ActionOpCodeParser
+from OpCodeUtilities import OpCodeUtilities
 
 class ServerlessDeviceConnection :
 
@@ -16,11 +17,34 @@ class ServerlessDeviceConnection :
 	def __init__(self):
 		return
 
+	def SendDataToClient(self, data, IP) :
+		try :
+			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.sock.settimeout(2)
+			self.sock.connect((IP, self.TCP_PORT))
+			self.sock.send(data)
+			data = self.sock.recv(self.BUFFER_SIZE)
+			return data
+		except :
+			print 'Failed to connect to client interrupt server'
+			return 'Failed'
+
+	def SendDataDirectlyToClientIP(self, outData) :
+		clientInterruptCommandByteArray = [chr(0x0A), chr(0x02), chr(outData.inputID), chr(outData.value)]
+		clientInterruptCommand = OpCodeUtilities().GetStringFromByteArray(clientInterruptCommandByteArray)
+		return self.SendDataToClient(clientInterruptCommand, outData.destinationIP)
+
+	def SendOutput(self, outputID, value) :
+		outputList = self.clientData.QueueOutput(outputID, value)
+		print len(outputList)
+
+		for x in range(0, len(outputList)) :
+			self.SendDataDirectlyToClientIP(outputList[x])
+
 	def SetDeviceData(self, deviceData) :
 		self.deviceData = deviceData
 
 	def ParseReceivedData(self, data) :
-
 		retVal = ActionOpCodeParser().GetActionOpCodeFromByteArray(data, self.deviceData)
 		return retVal
 
