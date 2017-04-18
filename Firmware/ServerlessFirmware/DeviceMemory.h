232de96e5f7319b7826bd0d0fa9c1ca5a675fda2
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-04-18 07:34:07 -0400

    #184 Changed Client to Device

--- Firmware/ServerlessFirmware/DeviceMemory.h
+++ Firmware/ServerlessFirmware/DeviceMemory.h
@@ -6,7 +6,7 @@ const char ControlOpCode = 0x02;
 const char VertexOpCode = 0x03;
 const char IconIDOpCode = 0x04;
 const char CustomIconDrawingOpCode = 0x05;
-const char ClientNameOpCode = 0x06;
+const char DeviceNameOpCode = 0x06;
 const char FrontEndPositionOpCOde = 0x07;
 const char ClientIPOpCode = 0x08;
 
@@ -28,12 +28,12 @@ int curFilledMemory = 0; // Indicate the curent filled memory.
 						 // Also serve as a place holder to 
 						 // show the back of allocated memory
 
-void GetClientIDOctets(unsigned long clientID, char &octet4, char &octet3, char &octet2, char &octet1)
+void GetDeviceIDOctets(unsigned long deviceID, char &octet4, char &octet3, char &octet2, char &octet1)
 {
-	octet1 = clientID%256;
-	octet2 = (clientID >> 8)%256;
-	octet3 = (clientID >> 16)%256;
-	octet4 = (clientID >> 24)%256;
+	octet1 = deviceID%256;
+	octet2 = (deviceID >> 8)%256;
+	octet3 = (deviceID >> 16)%256;
+	octet4 = (deviceID >> 24)%256;
 }
 
 void AddNewCharToMemory(char newMem)
@@ -42,24 +42,26 @@ void AddNewCharToMemory(char newMem)
 	curFilledMemory++;
 }
 
-void SetClientNameInMemory(char* clientName, int numCharacters, unsigned long clientID)
+void SetDeviceNameInMemory(char* deviceName, int numCharacters, unsigned long deviceID)
 {
-	char ClientID4 = 0;
-	char ClientID3 = 0;
-	char ClientID2 = 0;
-	char ClientID1 = 0;
-	GetClientIDOctets(clientID, ClientID4, ClientID3, ClientID2, ClientID1);
+	char DeviceID4 = 0;
+	char DeviceID3 = 0;
+	char DeviceID2 = 0;
+	char DeviceID1 = 0;
+	GetDeviceIDOctets(deviceID, DeviceID4, DeviceID3, DeviceID2, DeviceID1);
 
-	AddNewCharToMemory(ClientNameOpCode);
-	AddNewCharToMemory(ClientID4);
-	AddNewCharToMemory(ClientID3);
-	AddNewCharToMemory(ClientID2);
-	AddNewCharToMemory(ClientID1);
+	AddNewCharToMemory(DeviceNameOpCode);
+	AddNewCharToMemory(DeviceID4);
+	AddNewCharToMemory(DeviceID3);
+	AddNewCharToMemory(DeviceID2);
+	AddNewCharToMemory(DeviceID1);
 	AddNewCharToMemory((char)numCharacters);
 
 	for(int i = 0; i < numCharacters; i++)
 	{
-		AddNewCharToMemory(clientName[i]);
+		AddNewCharToMemory(deviceName[i]);
 	}
 }
+
+
 
