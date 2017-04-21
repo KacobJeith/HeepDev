0cc9ba7759c46b1d37692d1fa51ab5007c67751d
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-04-21 12:02:49 -0400

    #212 Added ENC communication library

---
+++ Firmware/ServerlessFirmware/ENC28J60_HeepComms.h
@@ -0,0 +1,34 @@
+#include <UIPEthernet.h>
+
+int TCP_PORT = 5000;
+EthernetServer server = EthernetServer(TCP_PORT);
+EthernetClient client;
+
+void CreateInterruptServer()
+{
+	uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x04,0x08};
+  	Ethernet.begin(mac);
+	serverIP = Ethernet.gatewayIP();
+
+	server.begin();
+}
+
+void CheckServerForInputs(char* inputBuffer)
+{
+	if (EthernetClient client = server.available())
+    {
+    	size_t size;
+	  	while((size = client.available()) > 0)
+	    {
+	      uint8_t* msg = (uint8_t*)malloc(size);
+	      size = client.read(msg,size);
+	      for(int i = 0; i < size; i++)
+	      {
+	      	inputBuffer[i] = msg[i];
+	      }
+	      free(msg);
+	  	}
+		  client.println("DATA from Server!");
+		  client.stop();
+    }
+}
