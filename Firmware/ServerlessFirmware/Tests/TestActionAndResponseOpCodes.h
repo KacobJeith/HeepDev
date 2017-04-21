1fc50034272d0db52ce8bfb5137dbb8fc367f8af
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-04-21 07:17:06 -0400

    #201 Crated a test for the memory dump ROP

--- Firmware/ServerlessFirmware/Tests/TestActionAndResponseOpCodes.h
+++ Firmware/ServerlessFirmware/Tests/TestActionAndResponseOpCodes.h
@@ -22,8 +22,41 @@ void TestClearOutputBufferAndAddChar()
 	CheckResults(TestName, valueList, 2);
 }
 
+void TestMemoryDumpROP()
+{
+	std::string TestName = "Memory Dump ROP";
+
+	ClearDeviceMemory();
+	SetDeviceIDAndName(0x01020304, "Jacob");
+	ClearOutputBuffer();
+	FillOutputBufferWithMemoryDump();
+
+	ExpectedValue valueList[10];
+	valueList[0].valueName = "Memory Dump";
+	valueList[0].expectedValue = MemoryDumpOpCode;
+	valueList[0].actualValue = outputBuffer[0];
+
+	valueList[1].valueName = "Device ID 1";
+	valueList[1].expectedValue = 1;
+	valueList[1].actualValue = deviceMemory[1];
+
+	valueList[2].valueName = "Device ID 2";
+	valueList[2].expectedValue = 2;
+	valueList[2].actualValue = deviceMemory[2];
+
+	valueList[3].valueName = "Device ID 3";
+	valueList[3].expectedValue = 3;
+	valueList[3].actualValue = deviceMemory[3];
+
+	valueList[4].valueName = "Device ID 4";
+	valueList[4].expectedValue = 4;
+	valueList[4].actualValue = deviceMemory[4];
+
+	CheckResults(TestName, valueList, 5);
+}
 
 void TestActionAndResponseOpCodes()
 {
 	TestClearOutputBufferAndAddChar();
+	TestMemoryDumpROP();
 }
