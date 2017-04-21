56c04b25c7d1007c78fb16667c39428e1d678e91
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-04-21 10:31:06 -0400

    #208 Read Vertex from COP

--- Firmware/ServerlessFirmware/ActionAndResponseOpCodes.h
+++ Firmware/ServerlessFirmware/ActionAndResponseOpCodes.h
@@ -168,6 +168,28 @@ void ExecuteSetPositionOpCode()
 	FillOutputBufferWithSuccess(SuccessMessage, strlen(SuccessMessage));
 }
 
+void ExecuteSetVertexOpCode()
+{
+	unsigned int counter = 1;
+	unsigned char numBytes = GetNumberFromBuffer(inputBuffer, counter, 1);
+	unsigned long txID = GetNumberFromBuffer(inputBuffer, counter, 4);
+	unsigned long rxID = GetNumberFromBuffer(inputBuffer, counter, 4);
+	unsigned char txControl = GetNumberFromBuffer(inputBuffer, counter, 1);
+	unsigned char rxControl = GetNumberFromBuffer(inputBuffer, counter, 1);
+	HeepIPAddress vertexIP;
+	vertexIP.Octet4 = GetNumberFromBuffer(inputBuffer, counter, 1);
+	vertexIP.Octet3 = GetNumberFromBuffer(inputBuffer, counter, 1);
+	vertexIP.Octet2 = GetNumberFromBuffer(inputBuffer, counter, 1);
+	vertexIP.Octet1 = GetNumberFromBuffer(inputBuffer, counter, 1);
+
+	Vertex myVertex;
+	myVertex.rxID = rxID;
+	myVertex.txID = txID;
+	myVertex.rxControlID = rxControl;
+	myVertex.txControlID = txControl;
+	myVertex.rxIPAddress = vertexIP;
+}
+
 void ExecuteControlOpCodes()
 {
 	unsigned char ControlOpCode = inputBuffer[0];
@@ -182,6 +204,10 @@ void ExecuteControlOpCodes()
 	else if(ControlOpCode == SetPositionOpCode)
 	{
 		ExecuteSetPositionOpCode();
+	}
+	else if(ControlOpCode == SetVertexOpCode)
+	{
+		ExecuteSetVertexOpCode();
 	}
 }
 
