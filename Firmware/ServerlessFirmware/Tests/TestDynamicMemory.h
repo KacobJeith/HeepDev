7a5d2a9e84594f5ef8e1e0f074bfd4d78c6168e6
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-04-26 11:45:41 -0400

    #228 Added tests for middle and end of memory defragmentation

--- Firmware/ServerlessFirmware/Tests/TestDynamicMemory.h
+++ Firmware/ServerlessFirmware/Tests/TestDynamicMemory.h
@@ -727,9 +727,9 @@ void TestDefragmentDeviceMemory()
 	CheckResults(TestName, valueList, 3);
 }
 
-void TestDefragmentDeviceMemoryInMiddle()
+void TestDefragmentDeviceMemoryAtEnd()
 {
-	std::string TestName = "Test Defragmented Vertex in Middle";
+	std::string TestName = "Test Defragmented Vertex at End";
 
 	ClearDeviceMemory();
 
@@ -805,6 +805,48 @@ void TestDefragmentDeviceMemoryDeviceAtFront()
 	valueList[2].valueName = "Memory Filled after Defragmentation";
 	valueList[2].expectedValue = 23;
 	valueList[2].actualValue = curFilledMemory;
+
+	CheckResults(TestName, valueList, 3);
+}
+
+void TestDefragmentDeviceMemoryInMiddle()
+{
+	std::string TestName = "Test Defragmented Vertex in Middle";
+
+	ClearDeviceMemory();
+
+	Vertex theVertex;
+	theVertex.rxID = 0x01020304;
+	theVertex.txID = 0x05060708;
+	theVertex.rxControlID = 1;
+	theVertex.txControlID = 2;
+	HeepIPAddress theIP;
+	theIP.Octet4 = 192;
+	theIP.Octet3 = 168;
+	theIP.Octet2 = 1;
+	theIP.Octet1 = 150;
+	theVertex.rxIPAddress = theIP;
+
+
+	SetDeviceNameInMemory("Crowbar", 7, 0x01020304);
+	SetVertexInMemory(theVertex);
+	SetIPInMemory(theIP, 0x04030210);
+	ExpectedValue valueList [3];
+	unsigned int beforeDeletionMemory = curFilledMemory;
+	valueList[0].valueName = "Memory Filled Before Deletion";
+	valueList[0].expectedValue = 39;
+	valueList[0].actualValue = beforeDeletionMemory;
+
+	DeleteVertexAtPointer(13);
+	unsigned int afterDeletionMemory = curFilledMemory;
+	valueList[1].valueName = "Memory Filled After Deletion";
+	valueList[1].expectedValue = 39;
+	valueList[1].actualValue = afterDeletionMemory;
+
+	DefragmentMemory();
+	valueList[2].valueName = "Memory Filled after Defragmentation";
+	valueList[2].expectedValue = 23;
+	valueList[2].actualValue = curFilledMemory;
 	CheckResults(TestName, valueList, 3);
 }
 
@@ -830,4 +872,5 @@ void TestDynamicMemory()
 	TestDefragmentDeviceMemory();
 	TestDefragmentDeviceMemoryInMiddle();
 	TestDefragmentDeviceMemoryDeviceAtFront();
+	TestDefragmentDeviceMemoryAtEnd();
 }
