80e0e1aa5009c4a5072503a1a1043c6ebe39153b
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-05-01 07:17:31 -0400

    #233 Added a simultaion EEPROM

--- Firmware/ServerlessFirmware/Heep_API.h
+++ Firmware/ServerlessFirmware/Heep_API.h
@@ -1,5 +1,6 @@
 #include "Simulation_HeepComms.h"
 #include "Scheduler.h"
+#include "Simulation_NonVolatileMemory.h"
 
 void SendOutputByID(unsigned char controlID, unsigned int value)
 {
@@ -22,12 +23,33 @@ void SendOutputByID(unsigned char controlID, unsigned int value)
 enum Tasks {Defragment = 0, saveMemory = 1, CheckForSave = 2};
 void SetupHeepTasks()
 {
+	ScheduleTask(Defragment);
+	ScheduleTask(saveMemory);
+	ScheduleTask(CheckForSave);
+}	
+
+void SaveMemory()
+{
 
 }
 
 void PerformHeepTasks()
 {
-	// if Scheduled, defragment!
-	// if Scheduled, check your IP
-	// if Scheduled, check to save
+	if(IsTaskTime())
+	{
+		Tasks curTask = GetCurrentTask();
+
+		if(curTask == Defragment)
+		{
+			DefragmentMemory();
+		}
+		else if(curTask == saveMemory)
+		{
+
+		}
+		else if(curTask == CheckForSave)
+		{
+
+		}
+	}
 }
---
+++ Firmware/ServerlessFirmware/Simulation_NonVolatileMemory.h
@@ -0,0 +1,12 @@
+
+void SaveMemory(unsigned char* memoryBuffer, unsigned int bytesToWrite)
+{
+}
+
+void ClearMemory()
+{
+}
+
+void ReadMemory(unsigned char* memoryBuffer)
+{
+}
