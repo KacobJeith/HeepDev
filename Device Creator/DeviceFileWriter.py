cfd37d52e3a915ee010d05cf55e52de51b5bceea
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-05-22 11:04:01 -0400

    #272 Write control definitions to file

--- Device Creator/DeviceFileWriter.py
+++ Device Creator/DeviceFileWriter.py
@@ -36,6 +36,24 @@ class DeviceWriter:
 			fileDescriptor.write("char controlName" + str(x) + " [] = \"" + deviceInfo.controlArr[x].controlName + "\";\n")
 			fileDescriptor.write("Control control" + str(x) + ";\n")
 
+	def WriteControlDefinitions(self, fileDescriptor, deviceInfo) :
+		for x in range(0, len(deviceInfo.controlArr) ) :
+			fileDescriptor.write("	control" + str(x) + ".controlName = controlName" + str(x) + ";\n")
+			fileDescriptor.write("	control" + str(x) + ".controlID = " + str(deviceInfo.controlArr[x].controlID) + ";\n")
+			fileDescriptor.write("	control" + str(x) + ".controlDirection = " + str(deviceInfo.controlArr[x].controlDirection) + ";\n")
+			fileDescriptor.write("	control" + str(x) + ".controlType = " + str(deviceInfo.controlArr[x].controlType) + ";\n")
+			fileDescriptor.write("	control" + str(x) + ".highValue = " + str(deviceInfo.controlArr[x].highVal) + ";\n")
+			fileDescriptor.write("	control" + str(x) + ".lowValue = " + str(deviceInfo.controlArr[x].lowVal) + ";\n")
+			fileDescriptor.write("	control" + str(x) + ".curValue = " + str(deviceInfo.controlArr[x].curVal) + ";\n")
+			fileDescriptor.write("	AddControl(control" + str(x) + ");\n")
+									
+
+	def WriteSetupFunction(self, fileDescriptor, deviceInfo) :
+		fileDescriptor.write("void setup()\n")
+		fileDescriptor.write("{\n")
+		fileDescriptor.write("	SetupHeepDevice(" + deviceInfo.deviceName + ");\n")
+		self.WriteControlDefinitions(fileDescriptor, deviceInfo)
+
 	def WriteArduinoFile(self, deviceInfo, filePath) :
 
 		fileName = self.GetFileName(deviceInfo)
@@ -48,7 +66,7 @@ class DeviceWriter:
 		f.write("#include \"Heep_API.h\"\n")
 		f.write("char deviceName [] = \"" + deviceInfo.deviceName + "\";\n")
 		self.WriteControlNames(f, deviceInfo)
-
+		self.WriteSetupFunction(f, deviceInfo)
 		f.close()
 
 		return
