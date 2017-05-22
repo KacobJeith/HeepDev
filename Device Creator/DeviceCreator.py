92c71008b43c2362f2564050d168c3f76cd909df
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-05-22 10:18:41 -0400

    #272 Added script to copy necessary files

--- Device Creator/DeviceCreator.py
+++ Device Creator/DeviceCreator.py
@@ -1,4 +1,13 @@
 from DeviceInfo import DeviceInfo
+from DeviceFileWriter import DeviceWriter
+
+def GetFilePath() :
+	filePath = raw_input('Enter relative file path: ')
+	return filePath
+
+filePath = GetFilePath()
 
 theDeviceInfo = DeviceInfo()
 theDeviceInfo.GetDeviceInfo()
+
+DeviceWriter(theDeviceInfo, filePath)
---
+++ Device Creator/DeviceFileWriter.py
@@ -0,0 +1,25 @@
+import subprocess
+
+class DeviceWriter:
+
+	def __init__(self, deviceInfo, filePath) :
+		self.WriteScriptFile(deviceInfo, filePath)
+		return
+
+	def GetFileName(self, deviceInfo) :
+		return deviceInfo.deviceName + '.ino'
+
+	def WriteScriptFile(self, deviceInfo, filePath) :
+		fileName = self.GetFileName(deviceInfo)
+
+		pathToFiles = '../Firmware/ServerlessFirmware/'
+
+		f = open('CopyScript.sh', 'w')
+		f.write('#!/bin/bash\n')
+		f.write('echo \"Begin Copy Script\"\n')  
+		f.write('mkdir ' + filePath + fileName + '\n')
+		f.write('cp -r ' + pathToFiles + ' ' + filePath + fileName + '\n')
+		f.close()
+
+		subprocess.call("bash CopyScript.sh", shell=True)
+		return
