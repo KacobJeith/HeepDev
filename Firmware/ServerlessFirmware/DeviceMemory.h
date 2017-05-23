d3247e965be19678ea1f28d66e1d24ffe9e74df0
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-05-23 13:18:28 -0400

    #274 Fixed Update XY code

--- Firmware/ServerlessFirmware/DeviceMemory.h
+++ Firmware/ServerlessFirmware/DeviceMemory.h
@@ -259,10 +259,10 @@ void UpdateXYInMemory(int x, int y, unsigned long deviceID)
 
 	if(success == 0)
 	{
-		deviceMemory[XYMemPosition + 6] = (x >> 8)%256;
-		deviceMemory[XYMemPosition + 7] = (x%256);
-		deviceMemory[XYMemPosition + 8] = (y >> 8)%256;
-		deviceMemory[XYMemPosition + 9] = (y%256);
+		deviceMemory[XYMemPosition + ID_SIZE + 2] = (x >> 8)%256;
+		deviceMemory[XYMemPosition + ID_SIZE + 3] = (x%256);
+		deviceMemory[XYMemPosition + ID_SIZE + 4] = (y >> 8)%256;
+		deviceMemory[XYMemPosition + ID_SIZE + 5] = (y%256);
 	}
 	else
 	{
