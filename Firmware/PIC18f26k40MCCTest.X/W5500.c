4e1829c7799e979f1ed28f704e45600a62a804b8
Dylan <dylan.fashbaugh@gmail.com>
2017-06-09 21:13:03 -0400

    HEEP-352 Added a buffer filling function for experimentation. The XC8 compiler does not seem to like initializing arrays

--- Firmware/PIC18f26k40MCCTest.X/W5500.c
+++ Firmware/PIC18f26k40MCCTest.X/W5500.c
@@ -220,18 +220,40 @@ void W5500SoftwareReset()
     SetMR( curMR | 0x80 );
 }
 
+void FillBuf4(uint8_t* buf, uint8_t a, uint8_t b, uint8_t c, uint8_t d)
+{
+    buf[0] = a;
+    buf[1] = b;
+    buf[2] = c;
+    buf[3] = d;
+}
+
+void FillBuf6(uint8_t* buf, uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e, uint8_t f)
+{
+    buf[0] = a;
+    buf[1] = b;
+    buf[2] = c;
+    buf[3] = d;
+    buf[4] = e;
+    buf[5] = f;
+}
+
 uint8_t TestW5500RegisterWriting()
 {
     uint8_t writeSubBuff [4] = {255,255,255,0};
+    FillBuf4(writeSubBuff, 255, 255, 255, 0);
     uint8_t readSubBuf [4];
     
     uint8_t writeMACBuf [6] = {1,3, 4, 16, 25, 7};
+    FillBuf6(writeMACBuf, 1, 3, 4, 16, 25, 7);
     uint8_t readMACBuf[6];
     
     uint8_t writeSourceIPBuf [4] = {192,168,1,186};
+    FillBuf4(writeSourceIPBuf, 192, 168, 1, 186);
     uint8_t readSourceIPBuf [4];
     
     uint8_t writeGatewayBuf [4] = {192,168,1,1};
+    FillBuf4(writeGatewayBuf, 192, 168, 1, 186);
     uint8_t readGatewayBuf [4];
     
     uint8_t success = 0;
