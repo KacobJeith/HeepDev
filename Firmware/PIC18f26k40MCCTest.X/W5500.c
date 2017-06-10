b1772df5451f1ade6c61cce9d80851326ae668ea
Dylan <dylan.fashbaugh@gmail.com>
2017-06-10 00:53:04 -0400

    HEEP-352 Created a W5500 Defines file

--- Firmware/PIC18f26k40MCCTest.X/W5500.c
+++ Firmware/PIC18f26k40MCCTest.X/W5500.c
@@ -1,37 +1,8 @@
 #include "mcc_generated_files/mcc.h"
 #include "W5500.h"
+#include "W5500Defines.h"
 
 #define NUMBER_OF_SOCKETS 8
-
-// COMMON REGISTER
-// Mode Register (MR)
-#define MR 0x0000
-// Gateway Address (GAR) - 0x0001->0x0004
-#define GAR0 0x0001
-// Subnet Mask Address (SUBR) - 0x0005->0x0008
-#define SUBR0 0x0005
-// Source Hardware Address [MAC Address] (SHAR) - 0x0009->0x000E
-#define SHAR0 0x0009
-// Source IP Address (SIPR) - 0x000F->0x0012
-#define SIPR 0x000F
-// Phy configuration Register (PHYCFGR)
-#define PHYCFGR 0x002E
-
-// SOCKET REGISTERS
-// Socket n Mode
-#define Sn_MR 0x0000
-// Socket n Command
-#define Sn_CR 0x0001
-// Socket n Status
-#define Sn_SR 0x0003
-// Socket Destination IP [Sn_DIPR] - 0x000C -> 0c000F
-#define Sn_DIPR0 0x000C
-// Socket Destination Port [Sn_DPORT] - 0x0010 -> 0x0011 
-#define Sn_DPORT0 0x0010
-// Receive Buffer Size Register
-#define Sn_RXBUF_SIZE 0x001E
-// Send Buffer Size Register
-#define Sn_TXBUF_SIZE 0x001F
 
 #define SS_PIN_TRIS TRISCbits.TRISC0
 #define SS_PIN LATCbits.LATC0
