c6cf49cb9ee2ca31194d4a055aa0ef903d2d075f
Dylan <dylan.fashbaugh@gmail.com>
2017-06-10 00:50:09 -0400

    HEEP-352 Added names for wiznet socket ports

--- Firmware/PIC18f26k40MCCTest.X/W5500.c
+++ Firmware/PIC18f26k40MCCTest.X/W5500.c
@@ -18,6 +18,16 @@
 #define PHYCFGR 0x002E
 
 // SOCKET REGISTERS
+// Socket n Mode
+#define Sn_MR 0x0000
+// Socket n Command
+#define Sn_CR 0x0001
+// Socket n Status
+#define Sn_SR 0x0003
+// Socket Destination IP [Sn_DIPR] - 0x000C -> 0c000F
+#define Sn_DIPR0 0x000C
+// Socket Destination Port [Sn_DPORT] - 0x0010 -> 0x0011 
+#define Sn_DPORT0 0x0010
 // Receive Buffer Size Register
 #define Sn_RXBUF_SIZE 0x001E
 // Send Buffer Size Register
