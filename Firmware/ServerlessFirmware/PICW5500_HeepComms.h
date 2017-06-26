745d5945832c6feb829e13d92eea7e2c7dcba57a
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-06-26 06:23:58 -0400

    HEEP-377 Added code to send output buffer to an IP with w5500 and PIC

--- Firmware/ServerlessFirmware/PICW5500_HeepComms.h
+++ Firmware/ServerlessFirmware/PICW5500_HeepComms.h
@@ -24,42 +24,26 @@ void CheckServerForInputs()
 
 void SendOutputBufferToIP(struct HeepIPAddress destIP)
 {
-//	IPAddress clientIP(destIP.Octet4, destIP.Octet3, destIP.Octet2, destIP.Octet1);
-//
-//	long next = 200;
-//
-//	Serial.println(clientIP);
-//
-//	if (client.connect(clientIP,TCP_PORT))
-//	{
-//      	client.write(outputBuffer, outputBufferLastByte);
-//      	next = millis() + 200;
-//      	while(client.available()==0)
-//        {
-//        	long curTimeSigned = millis();
-//          	if (next - curTimeSigned < 0)
-//          	{
-//          		Serial.println("PastTimeout");
-//          		goto pastTimeout;
-//          	}
-//           
-//        }
-//
-//      	int size;
-//      	while((size = client.available()) > 0)
-//      	{
-//          	uint8_t* msg = (uint8_t*)malloc(size);
-//          	size = client.read(msg,size);
-//
-//          	for(int i = 0; i < size; i++)
-//          	{
-//          		inputBuffer[i] = msg[i];
-//          	}
-//
-//	     	free(msg);
-//      	}
-//pastTimeout:
-//      client.stop();
-//    }
+    uint8_t byteIP [4];
+    byteIP[0] = destIP.Octet4;
+    byteIP[1] = destIP.Octet3;
+    byteIP[2] = destIP.Octet2;
+    byteIP[3] = destIP.Octet1;
+    uint16_t myPort = 5000;
+    uint8_t destPort[2];
+    destPort[0] = myPort >> 8;
+    destPort[1] = myPort & 0xFF;
 
+    ConnectToIP(byteIP, destPort, 1);
+    SendData(outputBuffer, outputBufferLastByte, 1);
+    
+    // Wait for data available in timeout
+    uint16_t curData = 0;
+    do
+    {
+        curData = DataAvailable(1);
+    }while(curData == 0);
+ 
+    ReadData(inputBuffer, curData, 1);
+    Disconnect(1);
 }
