a1dcd8450eef15fb5199c327d58d4feae484e755
Dylan <dylan.fashbaugh@gmail.com>
2017-08-14 13:09:45 -0400

    HEEP-493 Added anchored place and group names

---
+++ FrontEnd/Unity/Heep Unity UI/Assets/Scripts/AddGroup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class AddGroup : MonoBehaviour, IPointerDownHandler {
+
+	public GameObject prefab;
+	public GameObject canvas;
+	public Button yourButton;
+
+	#region IPointerDownHandler implementation
+
+	public void OnPointerDown (PointerEventData eventData)
+	{
+		Debug.Log ("Mouse detected");
+
+		GameObject enemy = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+		enemy.transform.SetParent (canvas.transform, false);
+	}
+
+	#endregion
+}
