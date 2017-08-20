06093161547df29aa535eee8bcf2cb7345e4d154
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-08-19 23:33:20 -0400

    HEEP-512 Outline of Heep Device in CSharp

---
+++ FrontEnd/Unity/HeepDevice/HeepDevice/HeepDevice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Heep
+{
+	public class HeepDevice
+	{
+		private int FirmwareVersion = 1;
+
+		private List<Vertex> vertices = new List<Vertex>(); 
+		private List<Control> controls = new List<Control>();
+
+		private DeviceID myID = new DeviceID();
+
+		public HeepDevice ()
+		{
+			
+		}
+	}
+
+	class DeviceID
+	{
+		public DeviceID()
+		{
+
+		}
+	}
+
+	class Vertex
+	{
+		public Vertex()
+		{
+
+		}
+	}
+
+	class Control
+	{
+		public Control()
+		{
+
+		}
+	}
+}
+
---
+++ FrontEnd/Unity/HeepDevice/HeepDevice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Heep;
+
+namespace MainProgram
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			HeepDevice myDevice = new HeepDevice ();
+			Console.WriteLine ("Hello World!");
+		}
+			
+	}
+}
