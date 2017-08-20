639827419b9cc3ee2538782505130ad99bf442fa
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-08-20 00:16:06 -0400

    HEEP-512 Added device definition to control and vertices

--- FrontEnd/Unity/HeepDevice/HeepDevice/HeepDevice.cs
+++ FrontEnd/Unity/HeepDevice/HeepDevice/HeepDevice.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Heep
 {
 	public class HeepDevice
 	{
 		private const int FirmwareVersion = 1;
-		private const int DeviceIDSize = 4;
+
 
 		private List<Vertex> vertices = new List<Vertex>(); 
 		private List<Control> controls = new List<Control>();
 
-		private DeviceID myID = new DeviceID(DeviceIDSize);
+		private DeviceID myID = new DeviceID();
 
 		public HeepDevice ()
 		{
@@ -33,9 +35,10 @@ namespace Heep
 	public class DeviceID
 	{
 		int [] theID;
-		public DeviceID(int numBytes)
+		private const int DeviceIDSize = 4;
+
+		public DeviceID()
 		{
-			theID = new int[numBytes];
 		}
 
 		public int[] GetIDArray()
@@ -47,21 +50,54 @@ namespace Heep
 		{
 			theID = newID;
 		}
+
+		public int GetDeviceIDSize()
+		{
+			return DeviceIDSize;
+		}
 	}
 
 	class Vertex
 	{
-		public Vertex()
-		{
 
+		DeviceID _rxID;
+		DeviceID _txID;
+
+		int _rxControlID;
+		int _txControlID;
+		IPAddress _destIP;
+
+		public Vertex(DeviceID rxID, DeviceID txId, int rxControlID, int txControlID, IPAddress destIP)
+		{
+			_rxID = rxID;
+			_txID = txId;
+			_rxControlID = rxControlID;
+			_txControlID = txControlID;
+			_destIP = destIP;
 		}
 	}
 
 	class Control
 	{
-		public Control()
-		{
+		int _controlID;
+		int _controlDirection;
+		int _controlType;
+		int _highValue;
+		int _lowValue;
+		int _curValue;
+		int _controlFlags;
+		String _controlName;
 
+		public Control(int controlID, int controlDirection, int controlType, int highValue, int lowValue, int curValue, int controlFlags, String ControlName)
+		{
+			_controlID = controlID;
+			_controlDirection = controlDirection;
+			_controlType = controlType;
+			_highValue = highValue;
+			_lowValue = lowValue;
+			_curValue = curValue;
+			_controlFlags = controlFlags;
+			_controlName = ControlName;
 		}
 	}
 }
