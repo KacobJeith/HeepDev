d5137aba3b19e85061a94e3dd71c76157f6e8403
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-08-30 10:44:34 -0400

    HEEP-500 Added default return to command parser

--- FrontEnd/Unity/HeepDevice/HeepDevice/HeepParser.cs
+++ FrontEnd/Unity/HeepDevice/HeepDevice/HeepParser.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Heep
 {
 	public static class HeepParser
 	{
-		public static void ParseCommand()
+		public static List<byte> ParseCommand(List <byte> commandBuffer, HeepDevice theDevice)
 		{
+			if (commandBuffer [0] == HeepLanguage.IsHeepDeviceOpCode) {
+				// CALL IS HEEP DEVICE PARSER
+			} else if (commandBuffer [0] == HeepLanguage.SetValueOpCode) {
+				// CALL SET VAL PARSER
+			}
+
+			List<byte> defaultRet = new List<byte> ();
+			return defaultRet;
+		}
+
+		public static List<byte> ParseIsHeepDeviceCommand(List <byte> commandBuffer, HeepDevice theDevice)
+		{
+			List <byte> outputBuf = new List<byte>();
+			return outputBuf;
 		}
 	}
 }
