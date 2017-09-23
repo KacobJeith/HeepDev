ab3fbf76826a01e8706a9e23921a755393766a68
KacobJeith <jacobkeith42@gmail.com>
2017-09-23 11:23:09 -0400

    HEEP-543 Added a fill in User Page that will someday hole data and analytics about your user account

---
+++ Website/hosting/src/components/UserPage.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { withRouter } from 'react-router-dom'
+import * as actions from '../redux/actions'
+import * as database from '../redux/firebase'
+
+var mapStateToProps = (state) => ({
+  
+})
+
+class UserPage extends React.Component {
+  
+  
+  render() {
+    var inputs = {
+      video: {
+        src: "../src/assets/heepwink3_gradient.mov",
+        loop: true, 
+        autoPlay: true,
+        height: 400,
+        width: 400,
+        style: {
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)'
+        }
+      }
+    }
+
+    return  <video {...inputs.video}/>
+           
+  }
+  
+}
+
+var mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actions, dispatch)
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UserPage))
+
