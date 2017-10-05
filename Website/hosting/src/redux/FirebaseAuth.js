4c7a14c26ad5957a16751df750a07fd3014f6f65
KacobJeith <jacobkeith42@gmail.com>
2017-10-04 20:12:31 -0400

    removing unimplemented references

--- Website/hosting/src/redux/FirebaseAuth.js
+++ Website/hosting/src/redux/FirebaseAuth.js
@@ -81,8 +81,6 @@ export const initializeFirebase = () => {
 	    validateUser()
 	    
 	    VerifyUser()
-      	database.readUserSignals();
-      	database.downloadAssets();
 
 	    loadUserProviders()
 
