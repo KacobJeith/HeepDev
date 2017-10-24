e6466845d2593cb7a33962906737677c365614ba
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-10-24 16:43:43 -0400

    HEEP-494 Integrated global data store

---
+++ FrontEnd/Unity/HeepDevice/HeepDevice/HeepGlobalData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Heep
+{
+
+	public class HeepGlobalData {
+
+		public static HeepGlobalData store;
+
+		public List<Place> places = new List<Place>();
+		public List<Group> groups = new List<Group>();
+		public List<HeepDevice> devices = new List<HeepDevice>();
+		public List<Control> controls = new List<Control> ();
+
+		public string activePlace = "null";
+		public string activeGroup = "null";
+	}
+
+	public class Place {
+
+		public string name;
+		public string placeID;
+
+		public Place(string name, string placeID){}
+
+		public Place() {
+			name = "empty";
+			placeID = "empty";
+		}
+
+	}
+
+	public class Group {
+
+		public string name;
+		public string placeID;
+		public string groupID;
+
+		public Group(string name, string groupID, string placeID){}
+
+		public Group() {
+			name = "empty";
+			placeID = "empty";
+			groupID = "empty";
+		}
+	}
+}
+
--- FrontEnd/Unity/HeepDevice/HeepDevice/HeepParser.cs
+++ FrontEnd/Unity/HeepDevice/HeepDevice/HeepParser.cs
@@ -24,6 +24,7 @@ namespace Heep
 		{
 			if (ROPBuffer [0] == HeepLanguage.MemoryDumpOpCode) {
 				Console.WriteLine ("MEMORY DUMP!!!");
+
 			}
 		}
 
