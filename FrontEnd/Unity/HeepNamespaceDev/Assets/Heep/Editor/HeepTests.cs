d8d7044858dda5369d00a38c9ff9dcd33938a8bd
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-10-31 16:03:45 -0400

    HEEP-558 Created a Heep Namespace Dev Unity project

---
+++ FrontEnd/Unity/HeepNamespaceDev/Assets/Heep/Editor/HeepTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+using Heep;
+
+[TestFixture]
+public class HeepTests  {
+
+	[Test]
+	public static void TestNonVolatileMemory()
+	{
+		string TestDeviceName = "Test Device";
+
+		List <byte> ID = new List<byte>();
+		for(byte i = 0; i < 4; i++)
+		{
+			ID.Add(i);
+		}
+
+		DeviceID myID = new DeviceID(ID);
+		HeepDevice myDevice = new HeepDevice (myID);
+
+		myDevice.SetDeviceName (TestDeviceName);
+
+		List <byte> readMem = NonVolatileData.ReadMemoryFromFile ();
+
+		int counter = 1;
+		string foundName = HeepParser.parseDeviceNameMOP (readMem, ref counter);
+
+		Assert.IsTrue (foundName == TestDeviceName);
+	}
+
+}
