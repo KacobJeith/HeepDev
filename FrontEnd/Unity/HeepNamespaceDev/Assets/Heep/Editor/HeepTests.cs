4c8a5a205a55879054dbf00eb9fc4183b6a71c48
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-11-01 00:04:18 -0400

    HEEP-558 Added a name parser test

--- FrontEnd/Unity/HeepNamespaceDev/Assets/Heep/Editor/HeepTests.cs
+++ FrontEnd/Unity/HeepNamespaceDev/Assets/Heep/Editor/HeepTests.cs
@@ -31,4 +31,30 @@ public class HeepTests  {
 		Assert.IsTrue (foundName == TestDeviceName);
 	}
 
+	[Test]
+	public static void TestParseName()
+	{
+		string deviceName = "Unity";
+
+		List <byte> nameTest = new List<byte> ();
+		nameTest.Add (0x06);
+		nameTest.Add (0x01);
+		nameTest.Add (0x02);
+		nameTest.Add (0x03);
+		nameTest.Add (0x04);
+		nameTest.Add (0x05);
+
+		for(int i = 0; i < deviceName.Length; i++)
+		{
+			nameTest.Add ((byte)deviceName [i]);
+		}
+	
+		int counter = 1;
+
+		string foundName = HeepParser.parseDeviceNameMOP (nameTest, ref counter);
+
+		Assert.AreEqual (deviceName, foundName);
+		Assert.AreEqual (11, counter);
+	}
+
 }
