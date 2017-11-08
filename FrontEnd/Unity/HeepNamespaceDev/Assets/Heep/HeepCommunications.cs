996a2e980aa9ec1ec1b702eba08c533a5c3f945a
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-11-08 14:57:09 -0500

    HEEP-561 Unity UDP receiving works

--- FrontEnd/Unity/HeepNamespaceDev/Assets/Heep/HeepCommunications.cs
+++ FrontEnd/Unity/HeepNamespaceDev/Assets/Heep/HeepCommunications.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using UnityEngine;
 
 namespace Heep
 {
 	public class HeepCommunications
 	{
+		public static int PORT = 5000;
 
 		public static void StartHeepServer(HeepDevice device)
 		{
@@ -22,69 +24,38 @@ namespace Heep
 		public static string data = null;  
 
 		public static void StartListening(HeepDevice device) {  
-			// Data buffer for incoming data.  
-			byte[] bytes = new Byte[1024];  
 
-			byte [] IPAddrArray = {0, 0, 0, 0};
-			IPAddress theAddr = new IPAddress(IPAddrArray);
-			IPEndPoint localEndPoint = new IPEndPoint(theAddr, 5000);  
-
-			// Create a TCP/IP socket.  
-			Socket listener = new Socket(AddressFamily.InterNetwork,  
-				SocketType.Stream, ProtocolType.Tcp );  
-
-			// Bind the socket to the local endpoint and   
-			// listen for incoming connections.  
-			try {  
-				listener.Bind(localEndPoint);  
-				listener.Listen(10);  
-
-				// Start listening for connections.  
-				while (true) {  
-					Console.WriteLine("Waiting for a connection...");  
-					// Program is suspended while waiting for an incoming connection.  
-					Socket handler = listener.Accept(); 
-
-					Console.WriteLine("Data accepted");
-
-					data = null;  
-
-					bytes = new byte[1024];  
-					int bytesRec = handler.Receive(bytes); 
-					Console.WriteLine(bytesRec);
-					data += Encoding.ASCII.GetString(bytes,0,bytesRec); 
-
-					// Show the data on the console.  
-					Console.WriteLine( "Text received : {0}", data);  
-
-					List <byte> commandData = new List<byte> ();
-					for(int i = 0; i < bytesRec; i++)
-					{
-						commandData.Add(bytes[i]);
-					}
-
-					// Echo the data back to the client.  
-//					byte[] msg = Encoding.ASCII.GetBytes(data); 
+			UdpClient client = new UdpClient(PORT);
+			byte[] recData;
+			while (true)
+			{
+				try
+				{
+					IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+					recData = client.Receive(ref anyIP);
+					List <byte> commandData = new List<byte>(recData);
 					List <byte> fromparser = HeepParser.ParseCommand(commandData, device);
-					byte[] msg = new byte[fromparser.Count];
+					anyIP.Port = 5000;
 
+					Debug.Log("IP Address: " + anyIP.Address + " Port: " + anyIP.Port);
+					Debug.Log("UDP Data Received: " + Encoding.ASCII.GetString(commandData.ToArray(),0,commandData.Count));
+
+					String printableReturn = "";
 					for(int i = 0; i < fromparser.Count; i++)
 					{
-						msg[i] = fromparser[i];
+						printableReturn += fromparser[i] + " ";
 					}
+					Debug.Log("Sending: " + printableReturn);
 
+					UdpClient udpClientB = new UdpClient();
+					udpClientB.Send(fromparser.ToArray(), fromparser.Count, anyIP);
 
-					handler.Send(msg);  
-					handler.Shutdown(SocketShutdown.Both);  
-					handler.Close();  
-				}  
-
-			} catch (Exception e) {  
-				Console.WriteLine(e.ToString());  
-			}  
-
-			Console.WriteLine("\nPress ENTER to continue...");  
-			Console.Read();  
+				}
+				catch (Exception err)
+				{
+					Debug.Log("UDP Exception: " + err.ToString());
+				}
+			}
 
 		}  
 
