36d117ca035455aeae0e6c0de1c41e56fff429c2
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2017-11-08 11:59:30 -0500

    HEEP-561 Added a test scene for a fake device

---
+++ FrontEnd/Unity/HeepNamespaceDev/Assets/Scripts/FakeDevice.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Heep;
+
+public class FakeDevice : MonoBehaviour {
+
+	HeepDevice myDevice;
+
+	void CreateHeepDevice()
+	{
+		List <byte> ID = new List<byte>();
+		for(byte i = 0; i < 4; i++)
+		{
+			ID.Add(i);
+		}
+
+		DeviceID myID = new DeviceID(ID);
+		myDevice = new HeepDevice (myID);
+
+		Control theControl = Control.CreateControl (Control.CtrlInputOutput.input, Control.CtrlType.OnOff, "First");
+		myDevice.AddControl (theControl);
+		Control newControl = Control.CreateControl (Control.CtrlInputOutput.input, Control.CtrlType.OnOff, "Second");
+		myDevice.AddControl (newControl);
+		myDevice.SetDeviceName ("Unity");
+		HeepCommunications.StartHeepServer (myDevice);
+	}
+
+	// Use this for initialization
+	void Start () {
+		CreateHeepDevice ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
