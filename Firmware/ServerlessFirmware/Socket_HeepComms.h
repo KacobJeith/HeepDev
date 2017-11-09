776c6185f4a08f58e88d72bb39150b6c8b605f97
dfashbaugh <dylan.fashbaugh@gmail.com>
2017-11-09 00:06:39 -0500

    HEEP-568 Added a functional UDP server for linux sockets

--- Firmware/ServerlessFirmware/Socket_HeepComms.h
+++ Firmware/ServerlessFirmware/Socket_HeepComms.h
@@ -15,6 +15,7 @@
 #include <iostream>
 using namespace std;
 
+struct sockaddr_in si_me, si_other;
 int lastConnectFd = -1;
 char recvBuffer[1500];
 char respondedToLastConnect = 1;
@@ -35,7 +36,6 @@ void die(char *s)
 
 void CreateServer(int portno)
 {
-    struct sockaddr_in si_me, si_other;
      
     int s, i, recv_len;
     socklen_t slen = sizeof(si_other);
@@ -67,20 +67,18 @@ void CreateServer(int portno)
         fflush(stdout);
          
         //try to receive some data, this is a blocking call
-        if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+        if ((recv_len = recvfrom(s, recvBuffer, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
         {
             die("recvfrom()");
         }
          
+
+        respondedToLastConnect = 0;
+
         //print details of the client/peer and the data received
         printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-        printf("Data: %s\n" , buf);
+        printf("Data: %s\n" , recvBuffer);
          
-        //now reply the client with the same data
-        if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
-        {
-            die("sendto()");
-        }
     }
  
     close(s);
@@ -104,8 +102,11 @@ void CreateInterruptServer()
 
 void CheckServerForInputs()
 {
-    if(respondedToLastConnect == 0 && lastConnectFd >= 0)
+    if(respondedToLastConnect == 0)
     {
+      si_other.sin_port = htons(5000);
+      cout << "Time to respond to " << inet_ntoa(si_other.sin_addr) << " " << ntohs(si_other.sin_port) << endl;
+
         int n = 0;
 
         for(int i = 0; i < 200; i++)
@@ -114,9 +115,21 @@ void CheckServerForInputs()
         }
 
         ExecuteControlOpCodes();
-        n = write(lastConnectFd,outputBuffer,outputBufferLastByte);
-        if (n < 0) error("ERROR writing to socket");
-        close(lastConnectFd);
+
+        int s;
+        socklen_t slen = sizeof(si_other);
+
+        //create a UDP socket
+        if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+        {
+            die("socket");
+        }
+
+        //now reply the client with the same data
+        if (sendto(s, outputBuffer, outputBufferLastByte, 0, (struct sockaddr*) &si_other, slen) == -1)
+        {
+            die("sendto()");
+        }
 
         lastConnectFd = -1;
         respondedToLastConnect = 1;
