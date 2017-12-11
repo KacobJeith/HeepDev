14eb5d39a5ee07a3c760ee3b570825e29ccafa7e
KacobJeith <jacobkeith42@gmail.com>
2017-12-11 00:25:37 -0500

    HEEP-597 add index file

---
+++ FrontEnd/DeviceBuilder/src/index.jsx
@@ -0,0 +1,41 @@
+// import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import Immutable from 'immutable'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import {HashRouter as Router, Route} from 'react-router-dom'
+
+import reducers from './redux/reducers.js'
+import DeviceBuilder from './components/DeviceBuilder'
+
+const startState = {
+  deviceName: "DefaultDevice",
+  systemType: "Arduino",
+  numControls: 1,
+  controls: [{
+    controlName: 'default',
+    controlID: 0,
+    controlDirection: 0,
+    controlType: 1,
+    highValue: 100,
+    lowValue: 0,
+    curValue: 0
+  }]
+}
+
+export const initialState = Immutable.Map(startState)
+export const store = createStore(reducers, startState, applyMiddleware(thunk));
+
+render(
+  <Provider store={store}>
+    <Router >
+    	<div >
+    		<Route exact path="/" component={DeviceBuilder}/>
+    	</div>
+	</Router>
+  </Provider>,
+
+  document.getElementById('root')
+);
