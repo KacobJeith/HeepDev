30ac2a3d04b3145150e0afbd8813d8ce929ed6e7
Jacob Keith <jacobkeith42@gmail.com>
2017-12-19 17:01:29 -0500

    HEEP-639 add PoE system type and import file

--- FrontEnd/DeviceBuilder/src/utilities/SystemPHYCompatibilities.js
+++ FrontEnd/DeviceBuilder/src/utilities/SystemPHYCompatibilities.js
@@ -6,10 +6,12 @@ export const sys_phy_files = {
 		socket: 'Socket_HeepComms.h'
 	},
 	Arduino: {
-		// W5500: 'ArduinoW5500_HeepComms.h',
 		W5100: 'ArduinoW5100_HeepComms.h',
 		ENC28J60: 'ENC28j60_HeepComms.h'
 	},
+	PoE: {
+		W5500: 'POE32u4W5500_HeepComms.h'
+	}
 	PIC: {
 		W5500: 'PICW5500_HeepComms.h'
 	},
