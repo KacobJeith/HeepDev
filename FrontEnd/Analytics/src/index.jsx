0715d5cbf680f571bc140bf9d4cfcc1dd5685973
Jacob Keith <jacobkeith42@gmail.com>
2017-12-21 12:17:19 -0500

    HEEP-645 Create new FrontEnd firebase project to house analytics interface

---
+++ FrontEnd/Analytics/src/index.jsx
@@ -0,0 +1,33 @@
+// import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import Immutable from 'immutable'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import {HashRouter as Router, Route} from 'react-router-dom'
+
+import reducers from './redux/reducers.js'
+import BarChart from './components/BarChart.jsx'
+
+
+const startState = {
+}
+
+console.log(startState);
+
+export const initialState = Immutable.Map(startState)
+export const store = createStore(reducers, startState, applyMiddleware(thunk));
+
+render(
+  <Provider store={store}>
+    <Router >
+		<Route path="/" component={BarChart}/>
+	</Router>
+  </Provider>,
+
+  document.getElementById('root')
+);
+
+
+
