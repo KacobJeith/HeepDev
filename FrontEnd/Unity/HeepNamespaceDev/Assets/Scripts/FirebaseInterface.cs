9d542642ac49afdd85b130a8074e45d15d62f076
Jacob Keith <jacobkeith42@gmail.com>
2017-12-20 21:25:31 -0500

    HEEP-632 Add Firebase Asset Package to Fake Device,connect it to the new Heep Analytics Project, and write to the database as a hello world test

---
+++ FrontEnd/Unity/HeepNamespaceDev/Assets/Scripts/FirebaseInterface.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+
+public class FirebaseInterface : MonoBehaviour {
+
+	public static FirebaseInterface store;
+
+	void Awake () {
+
+		if (store == null) {
+			DontDestroyOnLoad (gameObject);
+			store = this;
+		} else if (store != this) {
+			Destroy (gameObject);
+		}
+
+	}
+
+	Firebase.Auth.FirebaseAuth auth;
+	Firebase.Auth.FirebaseUser user;
+
+	private string testUser = "waxjVMMDemMCfPrKKnXKMlhRofo1";
+	public bool editorTesting = false;
+
+	public string analyticsCredential = "Heep-Analytics-ba59624ad931.p12";
+	public string analyticsURL = "https://heep-analytics.firebaseio.com";
+	public string analyticsServiceAccountID = "unity-user@heep-analytics.iam.gserviceaccount.com";
+	private string p12Password = "notasecret";
+
+
+	void Start() {
+		// Set this before calling into the realtime database.
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(analyticsURL);
+		FirebaseApp.DefaultInstance.SetEditorP12FileName(Application.dataPath + "/FirebaseAssetPackage/Editor Default Resources/" + analyticsCredential);
+		FirebaseApp.DefaultInstance.SetEditorServiceAccountEmail(analyticsServiceAccountID);
+		FirebaseApp.DefaultInstance.SetEditorP12Password(p12Password);
+
+		InitializeFirebase ();
+		WriteHello ();
+
+	}
+
+
+	void InitializeFirebase() {
+		
+		auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+		auth.StateChanged += AuthStateChanged;
+		AuthStateChanged(this, null);
+	}
+
+	void WriteHello() {
+
+		DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+		if (editorTesting) {
+			Debug.Log ("Editor Testing");
+			reference.Child("users").Child(testUser).Child("username").SetValueAsync("Unity Tester");
+		}
+
+	}
+
+	void AuthStateChanged(object sender, System.EventArgs eventArgs) {
+		if (auth.CurrentUser != user) {
+			
+			bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+
+			if (!signedIn && user != null) {
+				Debug.Log("Signed out " + user.UserId);
+			}
+			user = auth.CurrentUser;
+
+			if (signedIn) {
+				Debug.Log("Signed in " + user.UserId);
+			}
+		}
+	}
+
+}
+
+
