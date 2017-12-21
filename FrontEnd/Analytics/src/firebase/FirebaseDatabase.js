ed5eb68bb225550ff1704508c01b15d391c7e154
Jacob Keith <jacobkeith42@gmail.com>
2017-12-21 16:38:05 -0500

    HEEP-645 add utilities for cleaner parsing import

--- FrontEnd/Analytics/src/firebase/FirebaseDatabase.js
+++ FrontEnd/Analytics/src/firebase/FirebaseDatabase.js
@@ -2,18 +2,34 @@ import firebase from 'firebase'
 import * as setup from '../index.jsx'
 import * as actions from '../redux/actions'
 
+import * as HAPI from '../heep/HeepConnections.js'
+
 
 export const readDeviceData = () => {
 	console.log("Reading Data")
 
 	firebase.database().ref('/data').on('value', function(snapshot) {
 
-		console.log("Read: ", snapshot.val())
+		console.log("Read: ", snapshot.val());
 
-		setup.store.dispatch(actions.addMemoryDump(snapshot.key, snapshot.val()));
+		var buffer = base64ToArrayBuffer(snapshot.val());
+		var data = HAPI.ConsumeMemoryDump(buffer);
+		
+		setup.store.dispatch(actions.addMemoryDump(snapshot.key, buffer));
 
 	})
 
+}
+
+const base64ToArrayBuffer = (base64) => {
+    var binary_string =  window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array( len );
+    for (var i = 0; i < len; i++)        {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+
+    return bytes;
 }
 
 
--- FrontEnd/Analytics/src/heep/HeepConnections.js
+++ FrontEnd/Analytics/src/heep/HeepConnections.js
@@ -3,8 +3,6 @@ import * as HAPIParser from './HAPIMemoryParser'
 import * as iconUtils from '../utilities/iconUtilities'
 import * as generalUtils from '../utilities/generalUtilities'
 import * as byteUtils from '../utilities/byteUtilities'
-const dgram = require('dgram');
-const udpServer = dgram.createSocket('udp4');
 
 var masterState = {
   devices: {deviceArray: []},
@@ -14,26 +12,6 @@ var masterState = {
   icons: {},
   url: ''
 };
-
-var heepPort = 5000;
-var searchComplete = false;
-var mostRecentSearch = {};
-
-export var SearchForHeepDevices = () => {
-  var gateway = findGateway();
-  var searchBuffer = Buffer.from([0x09, 0x00])
-
-  var client = dgram.createSocket("udp4");
-  client.bind(function(err, bytes){
-      console.log("Set Broadcast");
-      client.setBroadcast(true);
-      var address = generalUtils.joinAddress(gateway,255)
-      client.send(searchBuffer, 5000, address, function(err, bytes) {
-          console.log("Broadcast sent");
-          client.close();
-      });
-    });
-}
 
 export var GetCurrentMasterState = () => {
   return masterState
@@ -52,151 +30,16 @@ export var ResetMasterState = () => {
   return masterState
 }
 
-export var SendPositionToHeepDevice = (deviceID, position) => {
-
-  SetDevicePositionFromBrowser(deviceID, position);
-
-  var IPAddress = masterState.devices[deviceID].IPAddress;
-  var xPosition = byteUtils.GetValueAsFixedSizeByteArray(position.left, 2);
-  var yPosition = byteUtils.GetValueAsFixedSizeByteArray(position.top, 2);
-  var packet = xPosition.concat(yPosition);
-  var numBytes = [packet.length];
-
-  var messageBuffer = Buffer.from([0x0B].concat(numBytes, packet));
-  console.log('Connecting to Device ', deviceID + ' at IPAddress: ' + IPAddress);
-  console.log('Data packet: ', messageBuffer);
-  ConnectToHeepDevice(IPAddress, heepPort, messageBuffer)
-
-}
-
-export var SendValueToHeepDevice = (deviceID, controlID, newValue) => {
-  if (CheckIfNewValueAndSet(deviceID, controlID, newValue)){
-    var IPAddress = masterState.devices[deviceID].IPAddress;
-    var controlByteArray = byteUtils.GetByteArrayFromValue(controlID);
-    var valueByteArray = byteUtils.GetByteArrayFromValue(newValue);
-    var numBytes = [controlByteArray.length + valueByteArray.length];
-    var messageBuffer = Buffer.from([0x0A].concat(numBytes, controlByteArray, valueByteArray));
-    console.log('Connecting to Device ', deviceID + ' at IPAddress: ' + IPAddress);
-    console.log('Data Packet: ',  messageBuffer);
-    ConnectToHeepDevice(IPAddress, heepPort, messageBuffer)
-  }
-}
-
-export var SendVertexToHeepDevices = (vertex) => {
-  console.log('Received the following vertex to send to HeepDevice: ', vertex)
-
-  var IPAddress = masterState.devices[vertex.txDeviceID].IPAddress;
-  AddVertex(vertex);
-  var messageBuffer = PrepVertexForCOP(vertex, 0x0C);
-
-  console.log('Connecting to Device ', vertex.txDeviceID + ' at IPAddress: ' + IPAddress);
-  console.log('Data Packet: ',  messageBuffer);
-
-  ConnectToHeepDevice(IPAddress, heepPort, messageBuffer)
-}
-
-export var SendDeleteVertexToHeepDevices = (vertex) => {
-  console.log('Received the following vertex to delete from HeepDevice: ', vertex)
-
-  var IPAddress = masterState.devices[vertex.txDeviceID].IPAddress;
-  var messageBuffer = PrepVertexForCOP(vertex, 0x0D);
-  DeleteVertex(vertex);
-
-  console.log('Connecting to Device ', vertex.txDeviceID + ' at IPAddress: ' + IPAddress);
-  console.log('Data Packet: ',  messageBuffer);
-
-  ConnectToHeepDevice(IPAddress, heepPort, messageBuffer)
-}
-
-export var PrepVertexForCOP = (vertex, COP) => {
-  var txDeviceID = byteUtils.GetDeviceIDAsByteArray(vertex.txDeviceID);
-  var txControlID = byteUtils.GetValueAsFixedSizeByteArray(vertex.txControlID, 1);
-  var rxDeviceID = byteUtils.GetDeviceIDAsByteArray(vertex.rxDeviceID);
-  var rxControlID = byteUtils.GetValueAsFixedSizeByteArray(vertex.rxControlID, 1);
-  var rxIP = byteUtils.ConvertIPAddressToByteArray(vertex.rxIP);
-  var packet = txDeviceID.concat(rxDeviceID, txControlID, rxControlID, rxIP);
-  var numBytes = [packet.length];
-
-  return Buffer.from([COP].concat(numBytes, packet));
-}
-
-var CheckIfNewValueAndSet = (deviceID, controlID, newValue) => {
-  var thisControl = generalUtils.nameControl(deviceID, controlID);
-  if (masterState.controls[thisControl].CurCtrlValue == newValue){
-    return false
-  } else {
-    masterState.controls[thisControl].CurCtrlValue = newValue;
-    return true
-  }
-}
-
-export var findGateway = () => {
-  var networkInterfaces = os.networkInterfaces( );
-
-  for (var interfaces in networkInterfaces) {
-    for (var i = 0; i < networkInterfaces[interfaces].length; i++ ) {
-      console.log(networkInterfaces[interfaces][i])
-      if (networkInterfaces[interfaces][i].netmask == '255.255.255.0' ){
-        var activeAddress = networkInterfaces[interfaces][i].address;
-        var address = activeAddress.split('.');
-        var myIp = address.pop();
-        
-        console.log('Searching on gateway: ', address);
-        
-        return address
-      }
-    }
-  }
-  
-  console.log('Searching on gateway: ', address);
-  return address
-}
-
-var ConnectToHeepDevice = (IPAddress, port, message) => {
-
-  console.log(message);
-
-  var client = dgram.createSocket('udp4');
-  console.log("About to Send " + IPAddress);
-  client.send(message, heepPort, IPAddress, (err) => {
-    client.close();
-  });
-}
-
-udpServer.on('error', (err) => {
-  console.log(`server error:\n${err.stack}`);
-  udpServer.close();
-});
-
-udpServer.on('message', (msg, rinfo) => {
-  ConsumeHeepResponse(msg, rinfo.address, rinfo.port);
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-});
-
-udpServer.on('listening', () => {
-  const address = udpServer.address();
-  console.log(`server listening ${address.address}:${address.port}`);
-});
-
-console.log("About to bind");
-udpServer.bind(heepPort);
-console.log("Done binding");
-
-
-
-
-var ConsumeHeepResponse = (data, IPAddress, port) => {
-  console.log('Device found at address: ', IPAddress + ':' + port.toString());
+export var ConsumeMemoryDump = (data) => {
   console.log('Stringified Data: ', data.toString());
   console.log('Raw inbound Data: ', data);
-
-  mostRecentSearch[IPAddress] = true;
+  
   var HeepResponse = HAPIParser.ReadHeepResponse(data);
 
   if (HeepResponse != false){
     if (HeepResponse.op == 0x0F) {
       //Memory Dump
-      AddMemoryChunksToMasterState(HeepResponse.memory, IPAddress);
+      AddMemoryChunksToMasterState(HeepResponse.memory);
 
     } else if ( HeepResponse.op == 0x10) {
       //Success
@@ -213,12 +56,13 @@ var ConsumeHeepResponse = (data, IPAddress, port) => {
   }
 }
 
-var AddMemoryChunksToMasterState = (heepChunks, IPAddress) => {
+var AddMemoryChunksToMasterState = (heepChunks) => {
   console.log(heepChunks)
+
   for (var i = 0; i < heepChunks.length; i++) {
     try {
       if (heepChunks[i].op == 1){
-        AddDevice(heepChunks[i], IPAddress);
+        AddDevice(heepChunks[i]);
 
       } else if (heepChunks[i].op == 2){
         AddControl(heepChunks[i]);
@@ -288,7 +132,7 @@ var AddControl = (heepChunk) => {
   masterState.controls[tempCtrlName].deviceID = heepChunk.deviceID;
   var currentIndex = SetControlStructure(heepChunk.deviceID, tempCtrlName)
 
-  masterState.positions[heepChunk.deviceID][tempCtrlName] = SetControlPosition(heepChunk.deviceID, currentIndex, heepChunk.control.ControlDirection);
+  masterState.positions[heepChunk.deviceID][tempCtrlName] = {top: 0, left: 0};
   masterState.controls.connections[tempCtrlName] = [];
 }
 
@@ -309,54 +153,6 @@ var SetNullPosition = (deviceID) => {
   }
 
   masterState.positions[deviceID] = newPosition;
-}
-
-var SetDevicePosition = (heepChunk) => {
-  masterState.positions[heepChunk.deviceID].device = heepChunk.position;
-  RecalculateControlPositions(heepChunk.deviceID);
-}
-
-var SetDevicePositionFromBrowser = (deviceID, position) => {
-  var newPosition = {
-    top: parseInt(position.top),
-    left: parseInt(position.left)
-  }
-
-  masterState.positions[deviceID].device = newPosition;
-  RecalculateControlPositions(deviceID);
-}
-
-var RecalculateControlPositions = (deviceID) => {
-  var startingPositions = masterState.positions[deviceID];
-  for (var controlName in startingPositions){
-    if (controlName == 'device'){ 
-      continue 
-    }
-
-    UpdateControlPosition(deviceID, controlName);
-  }
-}
-
-var UpdateControlPosition = (deviceID, controlName) => {
-  var devicePosition = masterState.positions[deviceID].device;
-  var thisPosition = masterState.positions[deviceID][controlName];
-  var direction = masterState.controls[controlName].ControlDirection;
-
-  thisPosition.top = devicePosition['top'] + 45 + 1.5 + 25/2 + 55*(thisPosition.index - 1), 
-  thisPosition.left = direction == 0 ? devicePosition['left'] + 10 : devicePosition['left'] + 250;
-  
-}
-
-var SetControlPosition = (deviceID, index, direction) => {
-  var devicePosition = masterState.positions[deviceID]['device'];
-
-  var position = {
-    top: devicePosition['top'] + 45 + 1.5 + 25/2 + 55*(index - 1), 
-    left: direction == 0 ? devicePosition['left'] + 10 : devicePosition['left'] + 250,
-    index: index
-  }
-  
-  return position;
 }
 
 var SetControlStructure = (deviceID, controlID) => {
