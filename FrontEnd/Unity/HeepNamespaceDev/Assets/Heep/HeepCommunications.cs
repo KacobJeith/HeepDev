198eb6ccc7066fa67ac1c459fe2ff008f9c8bb54
KacobJeith <jacobkeith42@gmail.com>
2018-01-11 00:24:11 -0500

    HEEP-659 try fixing the tls exception by intercepting the certificate

--- FrontEnd/Unity/HeepNamespaceDev/Assets/Heep/HeepCommunications.cs
+++ FrontEnd/Unity/HeepNamespaceDev/Assets/Heep/HeepCommunications.cs
@@ -7,6 +7,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 namespace Heep
 {
@@ -108,6 +110,7 @@ namespace Heep
 		{
 			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
 			request.Method = "PUT";
+			ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
 
 			System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
 			Byte[] byteArray = encoding.GetBytes(jsonContent);
@@ -129,10 +132,39 @@ namespace Heep
 			}
 		}
 
+		static bool MyRemoteCertificateValidationCallback(System.Object sender,
+			X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+		{
+			bool isOk = true;
+			// If there are errors in the certificate chain,
+			// look at each error to determine the cause.
+			if (sslPolicyErrors != SslPolicyErrors.None) {
+				for (int i=0; i<chain.ChainStatus.Length; i++) {
+					if (chain.ChainStatus[i].Status == X509ChainStatusFlags.RevocationStatusUnknown) {
+						continue;
+					}
+					chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+					chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+					chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan (0, 1, 0);
+					chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+					bool chainIsValid = chain.Build ((X509Certificate2)certificate);
+					if (!chainIsValid) {
+						isOk = false;
+						break;
+					}
+				}
+			}
+			return isOk;
+		}
+
 
 		public HeepCommunications ()
 		{
 		}
+
+
 	}
+
+
 }
 
