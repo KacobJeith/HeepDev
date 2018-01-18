309da055f475657898610cca187b03db02c6fe04
Jacob Keith <jacobkeith42@gmail.com>
2018-01-18 15:41:47 -0500

    HEEP-666 removed some paper signals code

--- FrontEnd/Local/src/firebase/FirebaseAuth.js
+++ FrontEnd/Local/src/firebase/FirebaseAuth.js
@@ -81,9 +81,6 @@ export const initializeFirebase = () => {
 	    validateUser()
 	    
 	    VerifyUser()
-      	database.readUserSignals();
-      	database.downloadAssets();
-
 	    loadUserProviders()
 
 	  } else {
