e008f0d91fb114643770a1d87639c55cac5f7ff9
KacobJeith <jacobkeith42@gmail.com>
2018-01-25 13:14:27 -0500

    HEEP-686 Render Analytics with Plotly Chart

--- FrontEnd/Local/src/components/Analytics/AnalyticsList.js
+++ FrontEnd/Local/src/components/Analytics/AnalyticsList.js
@@ -3,14 +3,17 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { withRouter } from 'react-router-dom'
 import * as Actions from '../../redux/actions'
+import Plot from 'react-plotly.js'
 
 import AnalyticsCard from './AnalyticsCard'
 
 
 const mapStateToProps = (state, ownProps) => ({
-  numberElements: countMOPS(state, ownProps) ,
+  // numberElements: countMOPS(state, ownProps) ,
   controlIDSeries: getAnalyticsSeries(state, ownProps, 'controlID'),
   timeSeries: getAnalyticsSeries(state, ownProps, 'timeStamp'),
+  seriesValue: getAnalyticsSeries(state, ownProps, 'controlValue'),
+  seriesHoverText: getHoverText(state, ownProps),
   deviceID: ownProps.deviceID.toString()
 })
 
@@ -21,28 +24,68 @@ class AnalyticsList extends React.Component {
 
   render () {
     
-    var inputs = {
-      thisCard: {
-        deviceID: this.props.deviceID,
-        element: 0,
-        key: 0
-      }
+    // var inputs = {
+    //   thisCard: {
+    //     deviceID: this.props.deviceID,
+    //     element: 0,
+    //     key: 0
+    //   }
       
-    }
+    // }
 
-    var analyticsCards = [];
+    // var analyticsCards = [];
 
-    for (var i = 0; i < this.props.numberElements; i++) {
-      inputs.thisCard.element = i;
-      inputs.thisCard.key = "MOP_" + i.toString();
-      analyticsCards.push( <AnalyticsCard {...inputs.thisCard}/>);
-    }
+    // for (var i = 0; i < this.props.numberElements; i++) {
+    //   inputs.thisCard.element = i;
+    //   inputs.thisCard.key = "MOP_" + i.toString();
+    //   analyticsCards.push( <AnalyticsCard {...inputs.thisCard}/>);
+    // }
+
+    // return (
+    //   <div>
+    //     {analyticsCards} 
+    //   </div>
+    // );
+
+    var data = [
+      {
+        x: this.props.timeSeries,
+        y: this.props.controlIDSeries,
+        mode: 'lines+markers',
+        type: 'scatter',
+        text: this.props.seriesHoverText,
+        marker: {
+          color: this.props.seriesValue,
+          size: Array(this.props.seriesValue.length).fill(25)
+        }
+      }
+    ];
+
+    var lastDataTime = this.props.timeSeries[this.props.timeSeries.length - 1];
+    var startTime = this.props.timeSeries[0];
+
+    if (this.props.timeSeries.length > 32) {
+      startTime = this.props.timeSeries[this.props.timeSeries.length - 30];
+    } 
+    // startTime.setHours(lastDataTime.getHours() - 3);
 
     return (
-      <div>
-        {analyticsCards} 
-      </div>
-    );
+    <Plot
+      data={data} 
+
+      layout={{
+        yaxis: {
+          range: [-20, 20]
+        },
+        xaxis: {
+          range: [startTime.toString(), lastDataTime.toString()]
+        },
+        width: 700,
+        height: 400,
+        title: 'EscapeTheRoom Nuclear'
+      }}
+    />
+  );
 
   }
 }
@@ -66,6 +109,8 @@ const countMOPS = (state, ownProps) => {
 }
 
 const getAnalyticsSeries = (state, ownProps, key) => {
+
+  console.log("Seting Series: ", key);
   if ("analytics" in state) {
     if (ownProps.deviceID.toString() in state.analytics) {
       return Array.from(state.analytics[ownProps.deviceID], x => x[key])
@@ -73,4 +118,15 @@ const getAnalyticsSeries = (state, ownProps, key) => {
   }
 
   return []
+}
+
+const getHoverText = (state, ownProps) => {
+  var controlIDs = getAnalyticsSeries(state, ownProps, 'controlID');
+  var hoverText = [];
+  for (var i in controlIDs) {
+    hoverText.push(state.devices_firebase[ownProps.deviceID].controls[controlIDs[i]].controlName)
+  }
+  console.log("Set hoverText");
+
+  return hoverText
 }
