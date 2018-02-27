f06c4f99233948a7cc1c3e5df0af39a9cf89cf58
Jacob Keith <jacobkeith42@gmail.com>
2018-02-27 17:59:23 -0500

    HEEP-751 transitioned ProductCard to MUI, and built it fresh using the grid

--- Website/hosting/src/components/store/ProductCard.js
+++ Website/hosting/src/components/store/ProductCard.js
@@ -2,113 +2,91 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as Actions from '../../redux/actions'
-import { Grid, Typography }   from 'material-ui';
+import { Grid, Typography, IconButton, Badge }   from 'material-ui';
+import { AddShoppingCart } from 'material-ui-icons';
+import { withStyles, withTheme } from 'material-ui/styles';
 
 var mapStateToProps = (state, ownProps) => ({
   product: state.shopify[ownProps.productID],
   productID: ownProps.productID
 })
 
+const styles = theme => ({
+  card: {
+    maxWidth: 400,
+  }
+});
+
 class ProductCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       hover: false,
-      deletionHover: false
+      deletionHover: false,
+      countInCart: 0
     }
   }
 
   render() {
 
-    var useBuyButtonUI = false;
-
     const inputs = {
-      container: {
-        style: {
-          marginTop: useBuyButtonUI ? 0 : 25,
-          marginLeft: useBuyButtonUI ? 50 : 25,
-          position: "relative",
-          boxShadow: this.state.hover ? "0px 5px 15px #999" : "" ,
-          width: 300,
-          height: 300,
-          transition: "transform 400ms",
-          transform: this.state.hover ? "translate(0px, -5px)" : "translate(0px, 0px)"
-        },
-        id: String(this.props.product.id),
-        onMouseEnter: () => this.setState({hover: true}),
-        onMouseLeave: () => this.setState({hover: false})
-      },
-      addCard: {
-        style: {
-          height: 230,
-          width: 300,
-          backgroundColor: "black",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          opacity: 0.3,
-          boxShadow: this.state.hover ? "5px 5px 5px #111" : ""
-        }
-      },
-      imageContainer: {
-        style: {
-          position: 'relative',
-          height: 230,
-          width: 300,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }
-      },
+      
       image: {
         src: this.props.product.images[0].src,
         style: {
-          // // position: "absolute",
-          // left: 0,
-          // top: 0,
-          maxHeight: "calc(100vw / 4)",
+          height: "calc(25vw - 8px)",
           maxWidth: "100%"
         }
-      },
-      optionsBar: {
-        style: {
-          position: "absolute",
-          alignItems: "center",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          top: 230,
-          left: 0,
-          bottom: 0,
-          height: 70,
-          width: "100%",
-          backgroundColor: "lightGray",
-          color: "black",
-          //fontSize: 18,
-          cursor: 'pointer'
-        },
-        onClick: () => {this.props.addProductToCart(this.props.productID)}
       }
     }
 
     return (
       <Grid item xs={4}>
-        <Grid container alignItems='center' direction='column' spacing={0}>
-          <Grid item >
-            <img {...inputs.image}/>
+        <Grid container alignItems='stretch' direction='column' spacing={0}>
+          <Grid item>
+            <Grid container alignItems='center' direction='column' spacing={0}>
+              <Grid item>
+                <img {...inputs.image}/>
+              </Grid>
+            </Grid>
           </Grid>
           <Grid item>
-            <Typography align='center' 
-            style={{
-              backgroundColor:'black',
-              opacity: 0.25,
-              color:'white',
-              width:'100%'
-            }}>
-              {this.props.product.title}
-            </Typography>
+            <Grid container spacing={0} alignItems='center' style={{
+                  backgroundColor: this.props.theme.palette.primary.light
+                }}>
+              <Grid item xs={10}>
+                <Typography align='left' 
+                style={{
+                  color: this.props.theme.palette.primary.contrastText,
+                  paddingLeft: 10,
+                  opacity: 0.8
+                }}>
+                  {this.props.product.title}
+                </Typography>
+              </Grid>
+              <Grid item xs>
+                <IconButton>
+                    {this.state.countInCart > 0 ? 
+                      (<Badge color="secondary" badgeContent={this.state.countInCart} className={this.props.classes.margin}>
+                        <AddShoppingCart 
+                                              nativeColor={this.props.theme.palette.primary.contrastText}
+                                              onClick={() => {
+                                                this.props.addProductToCart(this.props.productID);
+                                                this.setState({countInCart: this.state.countInCart + 1});
+                                              }}/>
+                      </Badge>)
+                      : <AddShoppingCart 
+                      nativeColor={this.props.theme.palette.primary.contrastText}
+                      onClick={() => {
+                        this.props.addProductToCart(this.props.productID);
+                        this.setState({countInCart: this.state.countInCart + 1});
+                      }}/>}
+                </IconButton>
+              </Grid>
+
           </Grid>
         </Grid>
+      </Grid>
       </Grid>
 
     );
@@ -120,4 +98,4 @@ var mapDispatchToProps = (dispatch) => {
   return bindActionCreators(Actions, dispatch)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProductCard)
+export default connect(mapStateToProps, mapDispatchToProps)(withTheme()(withStyles(styles)(ProductCard)))
