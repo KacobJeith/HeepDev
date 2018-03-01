f495ec6b41c6f69ffd0a2dd3a98db2a3a8a9ae75
KacobJeith <jacobkeith42@gmail.com>
2018-02-28 23:51:11 -0500

    HEEP-755 adding collapsable menu to display device controls

--- Website/hosting/src/components/heep/DeviceCard.js
+++ Website/hosting/src/components/heep/DeviceCard.js
@@ -3,9 +3,21 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { withRouter } from 'react-router-dom'
 import * as actions from '../../redux/actions'
-import { ListItem, ListItemText, ListItemIcon, ListItemSecondaryAction, IconButton}  from 'material-ui'
-import { withTheme }       from 'material-ui/styles'
-import { Visibility }  from 'material-ui-icons'
+import { List,
+         ListItem, 
+         ListItemText, 
+         ListItemIcon, 
+         ListItemSecondaryAction, 
+         IconButton,
+         Divider,
+         Collapse }                 from 'material-ui'
+import { withTheme }                from 'material-ui/styles'
+import {  ExpandLess, 
+          ExpandMore, 
+          Redo,
+          Undo,
+          LinearScale,
+          PowerSettingsNew }   from 'material-ui-icons'
 
 var mapStateToProps = (state, ownProps) => ({
   device: state.devices[ownProps.deviceID]
@@ -15,8 +27,44 @@ class DeviceCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hover: false
+      hover: false,
+      view: false
     }
+  }
+
+  displayDeviceControls() {
+
+    var inputs = {
+      nested: {
+        style: { 
+          paddingLeft: this.props.theme.spacing.unit * 12,
+        }
+      }
+      
+    }
+
+    return (
+      <Collapse in={this.state.view} timeout="auto" unmountOnExit>
+        <div>
+          {this.props.device.controls.map((thisControl) => {
+
+            return(
+             <List disablePadding key={this.props.deviceID + thisControl.controlName}>
+               <ListItem  {...inputs.nested}>
+                 <ListItemIcon>
+                   {thisControl.controlDirection == 0 ? <Redo /> : <Undo/>}
+                 </ListItemIcon>
+                 <ListItemIcon>
+                   {thisControl.controlType == 1 ?  <LinearScale/> : <PowerSettingsNew /> }
+                 </ListItemIcon>
+                 <ListItemText inset secondary={thisControl.controlName} />
+               </ListItem>
+             </List>
+             )
+          })}
+        </div>
+      </Collapse>
+    )
   }
 
   render() {
@@ -26,6 +74,8 @@ class DeviceCard extends React.Component {
 
     var inputs = {
       item: {
+        button: true,
+        onClick: () => this.setState({view: !this.state.view}),
         onMouseEnter: () => this.setState({hover: true}),
         onMouseLeave: () => this.setState({hover: false}),
         style: {
@@ -39,30 +89,23 @@ class DeviceCard extends React.Component {
         },
         type:"image/svg+xml",
         data: "../src/assets/svg/" + name + ".svg"
-      },
-      deleteButton: {
-        'aria-label': 'Delete',
-        onClick: () => console.log('delete this device'),
-        onMouseEnter: () => this.setState({hover: true}),
-        onMouseLeave: () => this.setState({hover: false}),
       }
     }
 
     
     return (
-      <ListItem {...inputs.item}>
-        <ListItemIcon>
-            <object {...inputs.primary}/>
-        </ListItemIcon>
-        <ListItemText secondary={this.props.device.identity.name} />
-        {(this.state.hover ? 
-          <ListItemSecondaryAction>
-            <IconButton {...inputs.deleteButton}>
-              <Visibility />
-            </IconButton>
-          </ListItemSecondaryAction> : <ListItemSecondaryAction/> )}
-        
-      </ListItem>
+      <div>
+        <ListItem {...inputs.item}>
+          <ListItemIcon>
+              <object {...inputs.primary}/>
+          </ListItemIcon>
+          <ListItemText inset  primary={this.props.device.identity.name} />
+          {this.state.view ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
+        {this.displayDeviceControls()}
+        <Divider inset/>
+
+      </div>
     )
 
   }
