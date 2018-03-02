1d81738409b42c44d10d8a8c2593beaf6ff16226
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2018-03-01 23:02:41 -0500

    HEEP-756 Blew out everything except for simulation to start

--- FrontEnd/DeviceBuilder/src/utilities/PackageSourceFiles.js
+++ FrontEnd/DeviceBuilder/src/utilities/PackageSourceFiles.js
@@ -2,52 +2,23 @@ import fileDownload from 'react-file-download'
 import JSZip from 'jszip'
 import randomNumber from 'random-number-csprng'
 
-import ActionAndResponseOpCodes     from '../../../../Firmware/ServerlessFirmware/ActionAndResponseOpCodes.h'
-import Arduino_EEPROM               from '../../../../Firmware/ServerlessFirmware/Arduino_EEPROM.h'
-import Arduino_Timer                from '../../../../Firmware/ServerlessFirmware/Arduino_Timer.h'
-import ArduinoW5100_HeepComms       from '../../../../Firmware/ServerlessFirmware/ArduinoW5100_HeepComms.h'
-import CommonDataTypes              from '../../../../Firmware/ServerlessFirmware/CommonDataTypes.h'
-import Device                       from '../../../../Firmware/ServerlessFirmware/Device.h'
-import DeviceMemory                 from '../../../../Firmware/ServerlessFirmware/DeviceMemory.h'
-import ENC28J60_HeepComms           from '../../../../Firmware/ServerlessFirmware/ENC28J60_HeepComms.h'
-import ESP8266_HeepComms            from '../../../../Firmware/ServerlessFirmware/ESP8266_HeepComms.h'
-import ESP8266_NonVolatileMemory    from '../../../../Firmware/ServerlessFirmware/ESP8266_NonVolatileMemory.h'
-import Heep_API                     from '../../../../Firmware/ServerlessFirmware/Heep_API.h'
-import MemoryUtilities              from '../../../../Firmware/ServerlessFirmware/MemoryUtilities.h'
-import POE32u4W5500_HeepComms       from '../../../../Firmware/ServerlessFirmware/POE32u4W5500_HeepComms.h'
-import PICW5500_HeepComms           from '../../../../Firmware/ServerlessFirmware/PICW5500_HeepComms.h'
-import PICW5500_NonVolatileMemory   from '../../../../Firmware/ServerlessFirmware/PICW5500_NonVolatileMemory.h'
-import PICW5500_Timer               from '../../../../Firmware/ServerlessFirmware/PICW5500_Timer.h'
-import Scheduler                    from '../../../../Firmware/ServerlessFirmware/Scheduler.h'
-import Simulation_HeepComms         from '../../../../Firmware/ServerlessFirmware/Simulation_HeepComms.h'
-import Simulation_NonVolatileMemory from '../../../../Firmware/ServerlessFirmware/Simulation_NonVolatileMemory.h'
-import Simulation_Timer             from '../../../../Firmware/ServerlessFirmware/Simulation_Timer.h'
-import Socket_HeepComms             from '../../../../Firmware/ServerlessFirmware/Socket_HeepComms.h'
+import Simulation_HeepCommsH         from '../../../../Firmware/ServerlessFirmware/Simulation_HeepComms.h'
+import Simulation_NonVolatileMemoryH from '../../../../Firmware/ServerlessFirmware/Simulation_NonVolatileMemory.h'
+import Simulation_TimerH             from '../../../../Firmware/ServerlessFirmware/Simulation_Timer.h'
+
+import Simulation_HeepCommsCPP         from '../../../../Firmware/ServerlessFirmware/Simulation_HeepComms.cpp'
+import Simulation_NonVolatileMemoryCPP from '../../../../Firmware/ServerlessFirmware/Simulation_NonVolatileMemory.cpp'
+import Simulation_TimerCPP           from '../../../../Firmware/ServerlessFirmware/Simulation_Timer.cpp'
 
 import { sys_phy_files } from '../utilities/SystemPHYCompatibilities'
 
 var sourceFiles = {
-  ActionAndResponseOpCodes: ActionAndResponseOpCodes,
-  Arduino_EEPROM: Arduino_EEPROM,
-  Arduino_Timer: Arduino_Timer,
-  ArduinoW5100_HeepComms: ArduinoW5100_HeepComms,
-  POE32u4W5500_HeepComms: POE32u4W5500_HeepComms,
-  CommonDataTypes: CommonDataTypes,
-  Device: Device,
-  DeviceMemory: DeviceMemory,
-  ENC28J60_HeepComms: ENC28J60_HeepComms,
-  ESP8266_HeepComms: ESP8266_HeepComms,
-  ESP8266_NonVolatileMemory: ESP8266_NonVolatileMemory,
-  Heep_API: Heep_API,
-  MemoryUtilities: MemoryUtilities,
-  PICW5500_HeepComms: PICW5500_HeepComms,
-  PICW5500_NonVolatileMemory: PICW5500_NonVolatileMemory,
-  PICW5500_Timer: PICW5500_Timer,
-  Scheduler: Scheduler,
-  Simulation_HeepComms: Simulation_HeepComms,
-  Simulation_NonVolatileMemory: Simulation_NonVolatileMemory,
-  Simulation_Timer: Simulation_Timer,
-  Socket_HeepComms: Socket_HeepComms
+  Simulation_HeepCommsH: Simulation_HeepCommsH,
+  Simulation_NonVolatileMemoryH: Simulation_NonVolatileMemoryH,
+  Simulation_TimerH: Simulation_TimerH,
+  Simulation_HeepCommsCPP: Simulation_HeepCommsCPP,
+  Simulation_NonVolatileMemoryCPP: Simulation_NonVolatileMemoryCPP,
+  Simulation_TimerCPP: Simulation_TimerCPP
 }
 
 export const packageSourceFiles = (deviceDetails, controls) => {
@@ -55,33 +26,9 @@ export const packageSourceFiles = (deviceDetails, controls) => {
   console.log("Device: ", deviceDetails);
   console.log("Controls: ", controls);
   
-  var zip = packageUniversalFiles();
   var autoGenIncludes = '';
 
   switch (deviceDetails.systemType) {
-
-    case "Arduino" : 
-    case "PoE" :
-      zip = packageArduinoFiles(deviceDetails, controls, zip);
-      autoGenIncludes = getIncludes_Arduino(deviceDetails);
-      break
-
-    case "ESP8266" : 
-      zip = packageESP8266Files(deviceDetails, controls, zip);
-      autoGenIncludes = getIncludes_ESP8266(deviceDetails);
-      break
-
-    case "Linux" :
-
-      zip.file('Socket_HeepComms.h', Socket_HeepComms);
-      autoGenIncludes = getIncludes_Linux(deviceDetails);
-      break
-
-    case "PIC" :
-
-      zip = packagePICFiles(deviceDetails, controls, zip);
-      autoGenIncludes = getIncludes_PIC(deviceDetails);
-      break
 
     case "Simulation" :
 
@@ -110,51 +57,12 @@ export const packageSourceFiles = (deviceDetails, controls) => {
 
 }
 
-const packageUniversalFiles = () => {
-  var zip = new JSZip();
-
-  zip.file('ActionAndResponseOpCodes.h', ActionAndResponseOpCodes);
-  zip.file('CommonDataTypes.h', CommonDataTypes);
-  zip.file('Device.h', Device);
-  zip.file('DeviceMemory.h', DeviceMemory);
-  zip.file('Heep_API.h', Heep_API);
-  zip.file('MemoryUtilities.h', MemoryUtilities);
-  zip.file('Scheduler.h', Scheduler);
-
-  return zip
-}
-
-const packageArduinoFiles = (deviceDetails, controls, zip) => {
-
-  zip.file(deviceDetails.deviceName + ".ino", composeInoFile(deviceDetails, controls));
-
-  zip.file('Arduino_EEPROM.h', Arduino_EEPROM);
-  zip.file('Arduino_Timer.h', Arduino_Timer);
-
-  return zip
-}
-
-const packageESP8266Files = (deviceDetails, controls, zip) => {
-  zip.file(deviceDetails.deviceName + ".ino", composeInoFile(deviceDetails, controls));
-
-  zip.file('ESP8266_NonVolatileMemory.h', ESP8266_NonVolatileMemory);
-  zip.file('Arduino_Timer.h', Arduino_Timer);
-
-  return zip
-}
-
-const packagePICFiles = (deviceDetails, controls, zip) => {
-
-  zip.file('PICW5500_NonVolatileMemory.h', PICW5500_NonVolatileMemory);
-  zip.file('PICW5500_Timer.h', PICW5500_Timer);
-
-  return zip
-}
-
 const packageSimulationFiles = (deviceDetails, controls, zip) => {
 
-  zip.file('Simulation_NonVolatileMemory.h', Simulation_NonVolatileMemory);
-  zip.file('Simulation_Timer.h', Simulation_Timer);
+  zip.file('Simulation_NonVolatileMemory.h', Simulation_NonVolatileMemoryH);
+  zip.file('Simulation_Timer.h', Simulation_TimerH);
+  zip.file('Simulation_NonVolatileMemory.cpp', Simulation_NonVolatileMemoryCPP);
+  zip.file('Simulation_Timer.cpp', Simulation_TimerCPP);
 
   return zip
 }
@@ -518,29 +426,6 @@ const getCommsFileName = (deviceDetails) => {
   return sys_phy_files[sys][phy]
 }
 
-const getIncludes_Linux = (deviceDetails) => {
-  return `#include "` + getCommsFileName(deviceDetails) + `"
-#include "Simulation_NonVolatileMemory.h"
-#include "Simulation_Timer.h"`
-}
-
-const getIncludes_ESP8266 = (deviceDetails) => {
-  return `String SSID = "` + deviceDetails.ssid + `";
-String Password = "` + deviceDetails.ssidPassword + `";
-#include <string.h>
-#include "` + getCommsFileName(deviceDetails) + `"
-#include "ESP8266_NonVolatileMemory.h"
-#include "Arduino_Timer.h"`
-}
-
-const getIncludes_Arduino = (deviceDetails) => {
-  return `#include <string.h>
-#include "` + getCommsFileName(deviceDetails) + `"
-#include "Arduino_EEPROM.h"
-#include "Arduino_Timer.h"
-`
-}
-
 const getIncludes_Simulation = (deviceDetails) => {
   return `#include <string.h>
 #include "` + getCommsFileName(deviceDetails) + `"
@@ -548,9 +433,4 @@ const getIncludes_Simulation = (deviceDetails) => {
 #include "Simulation_Timer.h"`
 }
 
-const getIncludes_PIC = (deviceDetails) => {
-  return `#include "` + getCommsFileName(deviceDetails) + `"
-#include "PICW5500_NonVolatileMemory.h"
-#include "PICW5500_Timer.h"`
-}
 
