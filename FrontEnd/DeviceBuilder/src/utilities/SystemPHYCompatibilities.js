bf796d84c2531f3921fac3029cb0a6bac10f6108
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2018-03-02 00:06:03 -0500

    HEEP-756 Added a new level in the PHY hierarchy

--- FrontEnd/DeviceBuilder/src/utilities/SystemPHYCompatibilities.js
+++ FrontEnd/DeviceBuilder/src/utilities/SystemPHYCompatibilities.js
@@ -1,6 +1,8 @@
 export const sys_phy_files = {
 	Simulation: {
-		SimulationH: 'Simulation_HeepComms.h',
-		SimulationCPP: 'Simulation_HeepComms.cpp'
+		Simulation: {
+			SimulationH: 'Simulation_HeepComms.h',
+			SimulationCPP: 'Simulation_HeepComms.cpp'
+		}
 	}
 }
