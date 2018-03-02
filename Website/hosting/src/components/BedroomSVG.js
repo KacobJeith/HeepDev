aac6f28bfe920b88a016ebf7e9d226753b8533aa
yennysu <yenny@heep.io>
2018-03-02 00:28:22 -0500

    HEEP-757 added color hover for remote

--- Website/hosting/src/components/BedroomSVG.js
+++ Website/hosting/src/components/BedroomSVG.js
@@ -15,41 +15,85 @@ class BedroomSVG extends React.Component{
 	};
 
   componentDidMount() {
-    this.createBtnCactus();
+    this.createButtons();
   };
 
-  createBtnCactus(){
-    this.btnCactus = document.getElementById('buttonCactus');
-    this.btnCactus.addEventListener('click', this.animateCactus);
-    this.btnCactus.addEventListener('mouseover', this.hoverCactus)
+  createButtons(){
+    this.buttonRemote = document.getElementById('buttonRemote');
+    this.buttonRemote.addEventListener('mouseover', this.hoverRemote);
+
+    this.buttonPig = document.getElementById('buttonPig');
+
+    this.buttonDiary = document.getElementById('buttonDiary');
+
+    this.buttonSleep = document.getElementById('buttonSleep');
+
+    this.buttonDresserTop = document.getElementById('buttonDresserTop');
+
+    this.buttonDresserBottom = document.getElementById('buttonDresserBottom');
+
+    this.buttonCactus = document.getElementById('buttonCactus');
+    this.buttonCactus.addEventListener('mouseover', this.hoverCactus)
+
+    this.buttonFlower = document.getElementById('buttonFlower');
+
+    this.buttonPail = document.getElementById('buttonPail');
   }
 
-  animateCactus() {
-    this.wholeCactus = document.getElementById('cactus');
-    this.cactusBody = document.getElementById('cactusBody');
+  colorCactus() {
+  }
+
+  hoverRemote() {
+    this.remoteBottom = document.getElementById('remoteBottom');
+    this.remoteBody = document.getElementById('remoteBody');
+    this.remoteButtonLeft = document.getElementById('remoteButtonLeft');
+    this.remoteButtonRight = document.getElementById('remoteButtonRight');
+    this.remoteAntennaTop = document.getElementById('remoteAntennaTop');
+    this.remoteStickBottomLeft = document.getElementById('remoteStickBottomLeft');
+    this.remoteStickBottomRight = document.getElementById('remoteStickBottomRight');
+    this.remoteStickTopRight = document.getElementById('remoteStickTopRight');
+    this.remoteStickTopLeft = document.getElementById('remoteStickTopLeft');
+
+    var tl = new TimelineMax();
+    tl.to(this.remoteBottom, 1, {fill: '#333333', delay:0.6}, 0);
+    tl.to(this.remoteBody, 1, {fill: '#666666', delay:0.6}, 0);
+    tl.to(this.remoteButtonLeft, 1, {fill: '#CCCCCC', delay:0.6}, 0);
+    tl.to(this.remoteButtonRight, 1, {fill: '#CCCCCC', delay:0.6}, 0);
+    tl.to(this.remoteAntennaTop, 1, {fill: '#000', delay:0.6}, 0);
+    tl.to(this.remoteStickBottomLeft, 1, {fill: '#999999', delay:0.6}, 0);
+    tl.to(this.remoteStickBottomRight, 1, {fill: '#999999', delay:0.6}, 0);
+    tl.to(this.remoteStickTopRight, 1, {fill: '#666666', delay:0.6}, 0);
+    tl.to(this.remoteStickTopLeft, 1, {fill: '#666666', delay:0.6}, 0);
+    tl.to(this.remoteBottom, 1, {fill: '#333333', delay:0.6}, 1);
+    tl.to(this.remoteBody, 1, {fill: '#666666', delay:0.6}, 1);
+    tl.to(this.remoteBottom, 1, {fill: '#FFF', delay:0.6}, 1);
+    tl.to(this.remoteBody, 1, {fill: '#FFF', delay:0.6}, 1);
+    tl.to(this.remoteButtonLeft, 1, {fill: '#FFF', delay:0.6}, 1);
+    tl.to(this.remoteButtonRight, 1, {fill: '#FFF', delay:0.6}, 1);
+    tl.to(this.remoteAntennaTop, 1, {fill: '#FFF', delay:0.6}, 1);
+    tl.to(this.remoteStickBottomLeft, 1, {fill: '#FFF', delay:0.6}, 1);
+    tl.to(this.remoteStickBottomRight, 1, {fill: '#FFF', delay:0.6}, 1);
+    tl.to(this.remoteStickTopRight, 1, {fill: '#FFF', delay:0.6}, 1);
+    tl.to(this.remoteStickTopLeft, 1, {fill: '#FFF', delay:0.6}, 1);
+  };
+
+  hoverCactus() {
+    this.cactus = document.getElementById('cactus');
     this.cactusPotBody = document.getElementById('cactusPotBody');
     this.cactusPotLip = document.getElementById('cactusPotLip');
     this.cactusPotTop = document.getElementById('cactusPotTop');
-    console.log("clicked")
+    this.cactusBody = document.getElementById('cactusBody');
+
     var tl = new TimelineMax();
     tl.to(this.cactusBody, 1, {fill: '#8cc63f', delay:0.6}, 0);
     tl.to(this.cactusPotTop, 1, {fill: '#603813', delay:0.6}, 0);
     tl.to(this.cactusPotBody, 1, {fill: '#a55447', delay:0.6}, 0);
     tl.to(this.cactusPotLip, 1, {fill: '#965040', delay:0.6}, 0);
-    tl.to(this.wholeCactus, 1, {scale: 3}, 1);
-    tl.to(this.wholeCactus, 1, {x:300}, 2);
-    tl.to(this.wholeCactus, 1, {scale: 1}, 3);
-    tl.to(this.wholeCactus, 1, {x:-1}, 4);
-  }
-
-  hoverCactus() {
-    this.wholeCactus = document.getElementById('cactus');
-    var tl = new TimelineMax({
-      repeat: 2
-    });
-    tl.fromTo(this.wholeCactus, 0.3, {x:-1}, {x:1, ease:RoughEase.ease.config({strength:8, points:20, template:Linear.easeNone, randomize:false}) , clearProps:"x"})
-    console.log("hover")
-  }
+    tl.to(this.cactusBody, 1, {fill: '#FFF', delay:0.6}, 1);
+    tl.to(this.cactusPotTop, 1, {fill: '#FFF', delay:0.6}, 1);
+    tl.to(this.cactusPotBody, 1, {fill: '#FFF', delay:0.6}, 1);
+    tl.to(this.cactusPotLip, 1, {fill: '#FFF', delay:0.6}, 1);
+  };
 
   render() {
     const inputs = {
