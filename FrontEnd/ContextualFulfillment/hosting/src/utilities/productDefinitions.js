c2d7ef665d4139f599141f1c9825bea97ac55229
KacobJeith <jacobkeith42@gmail.com>
2018-03-05 16:33:02 -0500

    HEEP-769 add a filler program for the 'sense button' product

--- FrontEnd/ContextualFulfillment/hosting/src/utilities/productDefinitions.js
+++ FrontEnd/ContextualFulfillment/hosting/src/utilities/productDefinitions.js
@@ -24,5 +24,31 @@ export const productDefinitions = {
 				pinNumber: 14
 			}
 		]
+	},
+	Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC81MDg5NTkwNjQwNjgx: {
+		deviceDetails: {
+			deviceName: "QueueTest2",
+			iconSelected: 6,
+			numControls: 1,
+			physicalLayer: "wifi",
+			selectingIcon: false,
+			ssid: "ssid",
+			ssidPassword: "pword",
+			systemType: "ESP8266"
+		},
+		controls: [
+			{
+				analogOrDigital:"digital",
+				controlDirection: 0,
+				controlID: 0,
+				controlName:"yeyeye",
+				controlType: 0,
+				curValue: 0,
+				highValue: 100,
+				lowValue:0,
+				pinNegativeLogic: false,
+				pinNumber: 14
+			}
+		]
 	}
 }
