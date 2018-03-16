cc13710464e5e5ae03fc2545a4f87e2161c55a91
Jacob Keith <jacobkeith42@gmail.com>
2018-03-15 20:08:04 -0400

    HEEP-776 fixed icon overlay dragging

--- FrontEnd/Electron/src/components/MaterialUI/Device.js
+++ FrontEnd/Electron/src/components/MaterialUI/Device.js
@@ -56,7 +56,7 @@ class Device extends React.Component {
 
 
 		return (
-			<div >
+			<div style={{position:'relative'}}>
 				<object {...svgIcon}  />
 				<div {...this.props.draggingCallbacks} style={{position:'absolute', top:0, left:0, width:'100%', height:'100%', cursor:'grab'}}/>
 			</div>
