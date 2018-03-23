973c42dac93145c8206662e9ca37427a1f9cbf08
KacobJeith <jacobkeith42@gmail.com>
2018-03-23 13:57:25 -0400

    HEEP-801 set version to v0.1.0 and delete unused babel section of package.json

--- FrontEnd/Electron/src/electron_main.js
+++ FrontEnd/Electron/src/electron_main.js
@@ -11,6 +11,8 @@ const heepConnect = require('./serverside/heep/HeepConnections');
 const simulationDevice =  require('./serverside/simulationHeepDevice.js');
 
 const autoUpdater = require('electron-updater').autoUpdater;
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = "info";
 
 let mainWindow
 
@@ -115,6 +117,13 @@ electronApp.on('ready', function() {
   mainWindow.on('closed', function () {
     mainWindow = null
   })
+
+  log.info('Main Electron function');
+  autoUpdater.checkForUpdatesAndNotify();
+
+  autoUpdater.on('checking-for-update', function(info) {
+    log.info("inside checking for update: ", info);
+  });
 });
 
 electronApp.on('window-all-closed', function () {
