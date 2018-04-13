a8c742fdec873f3082833b2f08384e5fda85d12d
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2018-04-13 14:30:48 -0400

    HEEP-807 Added extra functions to express the many different ways to set a command and send data

--- Firmware/ServerlessFirmware/Heep_API.h
+++ Firmware/ServerlessFirmware/Heep_API.h
@@ -67,7 +67,14 @@ void AddRangeControl(char* controlName, int inputOutput, int highValue, int lowV
 void AddOnOffControl(char* controlName, int inputOutput, int startingValue);
 
 int GetControlValueByName(char* controlName);
+
 void SetControlValueByName(char* controlName, int newValue);
+void SetControlValueByNameAlwaysSend(char* controlName, int newValue);
+void SetControlValueByNameNoSend(char *controlName, int newValue);
+
+void SetControlValueByNameNoAnalytics(char *controlName, int newValue);
+void SetControlValueByNameNoAnalyticsNoSend(char *controlName, int newValue);
+void SetControlValueByNameNoAnalyticsAlwaysSend(char *controlName, int newValue);
 
 void SendControlsOnHeartBeat(unsigned long controlSendPeriod);
 
