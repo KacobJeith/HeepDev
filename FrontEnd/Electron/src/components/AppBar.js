6649023fc2a5ca0c93fecc8f83e8160253ce7096
Jacob Keith <jacobkeith42@gmail.com>
2018-04-13 14:53:55 -0400

    HEEP-960 use svg in appbar

--- FrontEnd/Electron/src/components/AppBar.js
+++ FrontEnd/Electron/src/components/AppBar.js
@@ -217,7 +217,7 @@ class AppBarDrawer extends React.Component {
 
     var inputs = {
       Logo: {
-        src: "https://firebasestorage.googleapis.com/v0/b/heep-3cddb.appspot.com/o/assets%2FLogo%2FSideBySide.png?alt=media&token=fa835081-275d-445e-be34-8241b08d687a",
+        src: 'src/assets/svg/SideBySide.svg',
         height: 50,
         style: {
           maxWidth: "250%",
