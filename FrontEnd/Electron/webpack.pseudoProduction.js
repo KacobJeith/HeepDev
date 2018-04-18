f6efc694685bdad292db7051dbf69f6cab13f20a
KacobJeith <jacobkeith42@gmail.com>
2018-04-18 18:06:27 -0400

    HEEP-990 revive npm run be as a useable feature as a middle ground between the production build and the development build

--- FrontEnd/Electron/webpack.pseudoProduction.js
+++ FrontEnd/Electron/webpack.pseudoProduction.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common.js');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
-   mode: 'production',
+   mode: 'development',
 
    output: {
      filename: '[name].[hash].bundle.js',
