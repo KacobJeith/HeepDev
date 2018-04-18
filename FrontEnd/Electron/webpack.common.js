bd3ff1f047971adee0d507d326effa13bd57daa3
KacobJeith <jacobkeith42@gmail.com>
2018-04-18 17:22:21 -0400

    HEEP-990 remove service worker from Electron App - it isn't necessary

--- FrontEnd/Electron/webpack.common.js
+++ FrontEnd/Electron/webpack.common.js
@@ -4,7 +4,6 @@ var path = require('path');
 var webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 module.exports = {
   target: 'web',
@@ -18,7 +17,6 @@ module.exports = {
   plugins: [
     new webpack.NamedModulesPlugin(),
     // new BundleAnalyzerPlugin(),
-    new SWPrecacheWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.join(__dirname, 'index_template.html')
