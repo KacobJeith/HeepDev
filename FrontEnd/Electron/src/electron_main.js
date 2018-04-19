75edf1d5347ec9d09b20919668dd2e44b3b6a658
KacobJeith <jacobkeith42@gmail.com>
2018-04-19 12:36:36 -0400

    HEEP-991 add a default menu to the electron app that lets user know what version is running

--- FrontEnd/Electron/src/electron_main.js
+++ FrontEnd/Electron/src/electron_main.js
@@ -1,9 +1,11 @@
 require('./heepServer')
-const electron = require('electron')
+const electron = require('electron');
+const { Menu } = require('electron')
 const electronApp = electron.app
 const BrowserWindow = electron.BrowserWindow
 var log = require('electron-log');
 
+const setupMenu = require('./electron/menu');
 
 const autoUpdater = require('electron-updater').autoUpdater;
 autoUpdater.logger = log;
@@ -11,6 +13,136 @@ autoUpdater.logger.transports.file.level = "info";
 
 let mainWindow
 
+const template = [
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click (item, focusedWindow) {
+          if (focusedWindow) focusedWindow.reload()
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+        click (item, focusedWindow) {
+          if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+        }
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'resetzoom'
+      },
+      {
+        role: 'zoomin'
+      },
+      {
+        role: 'zoomout'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'togglefullscreen'
+      }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {
+        role: 'minimize'
+      },
+      {
+        role: 'close'
+      }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternal('https://heep.io') }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  const name = electronApp.getName()
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        role: 'about'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'hide'
+      },
+      {
+        role: 'hideothers'
+      },
+      {
+        role: 'unhide'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'quit'
+      }
+    ]
+  })
+  // Edit menu.
+  template[1].submenu.push(
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Speech',
+      submenu: [
+        {
+          role: 'startspeaking'
+        },
+        {
+          role: 'stopspeaking'
+        }
+      ]
+    }
+  )
+  // Window menu.
+  template[3].submenu = [
+    {
+      label: 'Close',
+      accelerator: 'CmdOrCtrl+W',
+      role: 'close'
+    },
+    {
+      label: 'Minimize',
+      accelerator: 'CmdOrCtrl+M',
+      role: 'minimize'
+    },
+    {
+      label: 'Zoom',
+      role: 'zoom'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Bring All to Front',
+      role: 'front'
+    }
+  ]
+}
 
 electronApp.on('ready', function() {
 
@@ -23,12 +155,13 @@ electronApp.on('ready', function() {
     })
 
   mainWindow.loadURL('http://localhost:3004/');
-  mainWindow.setMenu(null);
-  // mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     mainWindow = null
   })
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
 
   log.info('Main Electron function');
   autoUpdater.checkForUpdatesAndNotify();
@@ -49,3 +182,4 @@ electronApp.on('activate', function () {
     createWindow()
   }
 })
+
