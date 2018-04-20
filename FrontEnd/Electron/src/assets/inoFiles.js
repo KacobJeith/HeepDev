45ce90691e2b6a6fad83b57e72a2b56f046b1cb1
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2018-04-20 17:12:08 -0400

    HEEP-766 Added Dylan file paths

--- FrontEnd/Electron/src/assets/inoFiles.js
+++ FrontEnd/Electron/src/assets/inoFiles.js
@@ -1,4 +1,10 @@
 import ClimateSensorInoFile from '../../../../../Heep Product Demos/ClimateSensorGitRepo/ClimateSensor/ClimateSensor.ino'
+import CloseOpenSensorInoFile from '../../../../../Heep Product Demos/CloseOpenSensorGitRepo/CloseOpenSensor/CloseOpenSensor.ino'
+import EncoderInoFile from '../../../../../Heep Product Demos/EncoderReadGitRepo/Encoder/Encoder.ino'
+import PiezoBuzzerInoFile from '../../../../../Heep Product Demos/PiezoBuzzerGitRepo/PiezoBuzzer/PiezoBuzzer.ino'
+import PassiveIRInoFile from '../../../../../Heep Product Demos/PIRGitRepo/PassiveIR/PassiveIR.ino'
+import ServoInoFile from '../../../../../Heep Product Demos/ServoGitRepo/Servo/Servo.ino'
+import SoilSensorInoFile from '../../../../../Heep Product Demos/SoilSensorGitRepo/SoilHygrometer/SoilHygrometer.ino'
 
 export const inoFiles = {
   ClimateSensorFile: ClimateSensorInoFile,
